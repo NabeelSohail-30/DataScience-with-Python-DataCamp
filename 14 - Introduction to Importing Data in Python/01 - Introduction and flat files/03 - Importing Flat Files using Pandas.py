44f3cb97e6ac7f97252f4d507608d1c42492b8fe
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-21 00:46:59 +0500

    refactor code

--- 14 - Introduction to Importing Data in Python/01 - Introduction and flat files/03 - Importing Flat Files using Pandas.py
+++ 14 - Introduction to Importing Data in Python/01 - Introduction and flat files/03 - Importing Flat Files using Pandas.py
@@ -1,41 +1,27 @@
-# Import pandas as pd
 import pandas as pd
-
-# Assign the filename: file
-file = 'titanic.csv'
-
-# Read the file into a DataFrame: df
-df = pd.read_csv(file)
-
-# View the head of the DataFrame
-print(df.head())
-
-# Assign the filename: file
-file = 'digits.csv'
-
-# Read the first 5 rows of the file into a DataFrame: data
-data = pd.read_csv(file, nrows=5, header=None)
-
-# Build a numpy array from the DataFrame: data_array
-data_array = np.array(data)
-
-# Print the datatype of data_array to the shell
-print(type(data_array))
-
-# Import matplotlib.pyplot as plt
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Assign filename: file
-file = 'titanic_corrupt.txt'
+# Read Titanic data from a CSV file and view the first few rows
+titanic_file = 'titanic.csv'
+titanic_df = pd.read_csv(titanic_file)
+print("Titanic DataFrame head:")
+print(titanic_df.head())
 
-# Import file: data
-data = pd.read_csv(file, sep='\t', comment='#', na_values=['Nothing'])
+# Read a subset of rows from a CSV file and convert to a numpy array
+digits_file = 'digits.csv'
+data_subset = pd.read_csv(digits_file, nrows=5, header=None)
+data_array = np.array(data_subset)
+print("Datatype of data_array:", type(data_array))
 
-# Print the head of the DataFrame
-print(data.head())
+# Read corrupted data from a tab-separated text file and view the head
+corrupt_file = 'titanic_corrupt.txt'
+corrupt_data = pd.read_csv(corrupt_file, sep='\t', comment='#', na_values=['Nothing'])
+print("Corrupted DataFrame head:")
+print(corrupt_data.head())
 
-# Plot 'Age' variable in a histogram
-pd.DataFrame.hist(data[['Age']])
+# Plot a histogram of the 'Age' variable from the corrupted DataFrame
+pd.DataFrame.hist(corrupt_data[['Age']])
 plt.xlabel('Age (years)')
 plt.ylabel('count')
 plt.show()
