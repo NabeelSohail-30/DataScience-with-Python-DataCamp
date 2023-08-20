1f8847b56381885081ae52bb291cd2194a4c6e33
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-21 00:44:36 +0500

    refactor code

--- 14 - Introduction to Importing Data in Python/01 - Introduction and flat files/01 - Introduction.py
+++ 14 - Introduction to Importing Data in Python/01 - Introduction and flat files/01 - Introduction.py
@@ -1,20 +1,26 @@
-# Open a file: file
-file = open('moby_dick.txt', mode='r')
+def read_file(file_name):
+    # Open the file in read mode using a context manager
+    with open(file_name, mode='r') as file:
+        # Read and print the entire content of the file
+        file_contents = file.read()
+        print("File Contents:\n", file_contents)
 
-# Print it
-print(file.read())
+    # Check whether the file is closed after exiting the context
+    print("File Closed:", file.closed)
 
-# Check whether file is closed
-print(file.closed)
+    # Open the file again using a context manager
+    with open(file_name) as file:
+        # Read and print the first 3 lines
+        for _ in range(3):
+            line = file.readline()
+            if line:
+                print("Line:", line.strip())
+            else:
+                print("Reached end of file.")
 
-# Close file
-file.close()
+    # Check whether the file is closed after exiting the context
+    print("File Closed:", file.closed)
 
-# Check whether file is closed
-print(file.closed)
-
-# Read & print the first 3 lines
-with open('moby_dick.txt') as file:
-    print(file.readline())
-    print(file.readline())
-    print(file.readline())
+# Call the function with the file name
+file_name = '../data/moby_dick.txt'
+read_file(file_name)
