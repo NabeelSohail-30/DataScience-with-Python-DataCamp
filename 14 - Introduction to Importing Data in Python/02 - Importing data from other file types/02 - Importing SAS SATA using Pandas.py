e73eb6d84f4f2d3a1067160a12d82fa7ad05fd46
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 00:06:09 +0500

    rafcatoring

--- 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/02 - Importing SAS SATA using Pandas.py
+++ 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/02 - Importing SAS SATA using Pandas.py
@@ -1,14 +1,16 @@
-# Import sas7bdat package
+import pandas as pd
+import matplotlib.pyplot as plt
 from sas7bdat import SAS7BDAT
 
-# Save file to a DataFrame: df_sas
+# Load SAS file into a pandas DataFrame
 with SAS7BDAT('sales.sas7bdat') as file:
     df_sas = file.to_data_frame()
 
-# Print head of DataFrame
+# Print the head of the DataFrame
+print("Head of df_sas:")
 print(df_sas.head())
 
-# Plot histogram of DataFrame features (pandas and pyplot already imported)
+# Plot a histogram of a DataFrame feature
 pd.DataFrame.hist(df_sas[['P']])
 plt.ylabel('count')
 plt.show()
@@ -17,13 +19,14 @@ plt.show()
 # Import pandas
 import pandas as pd
 
-# Load Stata file into a pandas DataFrame: df
+# Load Stata file into a pandas DataFrame
 df = pd.read_stata('disarea.dta')
 
-# Print the head of the DataFrame df
+# Print the head of the DataFrame
+print("Head of df:")
 print(df.head())
 
-# Plot histogram of one column of the DataFrame
+# Plot a histogram of a column from the DataFrame
 pd.DataFrame.hist(df[['disa10']])
 plt.xlabel('Extent of disease')
 plt.ylabel('Number of countries')
