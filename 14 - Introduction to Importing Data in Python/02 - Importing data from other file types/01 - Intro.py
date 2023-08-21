f1f3bc733f2deed2afac2d8d2a4e141fa526bb79
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-21 23:44:47 +0500

    some code

--- 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/01 - Intro.py
+++ 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/01 - Intro.py
@@ -23,3 +23,15 @@ xls = pd.ExcelFile(file)
 
 # Print sheet names
 print(xls.sheet_names)
+
+# Load a sheet into a DataFrame by name: df1
+df1 = xls.parse('2004')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xls.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
