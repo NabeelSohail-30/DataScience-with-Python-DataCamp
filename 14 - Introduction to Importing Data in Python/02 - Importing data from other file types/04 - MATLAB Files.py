a2a6a58430f6f224e75b9d5b3be30194d7a6997c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 00:04:48 +0500

    rafcatoring

--- 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/04 - MATLAB Files.py
+++ 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/04 - MATLAB Files.py
@@ -1,26 +1,26 @@
-# Import package
 import scipy.io
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Load MATLAB file: mat
+# Load MATLAB file using scipy.io
 mat = scipy.io.loadmat('albeck_gene_expression.mat')
 
-# Print the datatype type of mat
-print(type(mat))
+# Print the datatype of mat
+print("Datatype of mat:", type(mat))
 
+# Print the keys present in the MATLAB dictionary
+print("Keys in the dictionary:", mat.keys())
 
-# Print the keys of the MATLAB dictionary
-print(mat.keys())
-
-# Print the type of the value corresponding to the key 'CYratioCyt'
-print(type(mat['CYratioCyt']))
+# Print the datatype of the value corresponding to the key 'CYratioCyt'
+print("Datatype of 'CYratioCyt' value:", type(mat['CYratioCyt']))
 
 # Print the shape of the value corresponding to the key 'CYratioCyt'
-print(np.shape(mat['CYratioCyt']))
+print("Shape of 'CYratioCyt' value:", np.shape(mat['CYratioCyt']))
 
 # Subset the array and plot it
 data = mat['CYratioCyt'][25, 5:]
 fig = plt.figure()
 plt.plot(data)
 plt.xlabel('time (min.)')
-plt.ylabel('normalized fluorescence (measure of expression)')
+plt.ylabel('normalized fluorescence (expression)')
 plt.show()
