4f7c5aabfef99235aec71f741956cab1e25ec32f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-21 23:58:53 +0500

    some code

--- 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/03 - HDF5 Files.py
+++ 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/03 - HDF5 Files.py
@@ -14,3 +14,26 @@ print(type(data))
 # Print the keys of the file
 for key in data.keys():
     print(key)
+
+
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = np.array(data['strain']['Strain'])
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
