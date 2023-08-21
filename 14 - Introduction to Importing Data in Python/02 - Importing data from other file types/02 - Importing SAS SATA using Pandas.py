1145efbf072d0127dfd07b4f575d787b2d3e3a84
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-21 23:53:06 +0500

    some code

--- 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/02 - Importing SAS SATA using Pandas.py
+++ 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/02 - Importing SAS SATA using Pandas.py
@@ -12,3 +12,19 @@ print(df_sas.head())
 pd.DataFrame.hist(df_sas[['P']])
 plt.ylabel('count')
 plt.show()
+
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
