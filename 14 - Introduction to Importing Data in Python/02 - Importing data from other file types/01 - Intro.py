74ab612150f19e7665c2216cb9115555d86d6cba
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-21 23:42:49 +0500

    some code

--- 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/01 - Intro.py
+++ 14 - Introduction to Importing Data in Python/02 - Importing data from other file types/01 - Intro.py
@@ -10,3 +10,16 @@ print(d)
 
 # Print datatype of d
 print(type(d))
+
+
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename: file
+file = 'battledeath.xlsx'
+
+# Load spreadsheet: xls
+xls = pd.ExcelFile(file)
+
+# Print sheet names
+print(xls.sheet_names)
