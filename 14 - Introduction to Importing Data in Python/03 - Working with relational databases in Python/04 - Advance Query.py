f67886d9c566a05512a3957064ca845db011f2c3
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 19:15:28 +0500

    refactor

--- 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/04 - Advance Query.py
+++ 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/04 - Advance Query.py
@@ -1,22 +1,28 @@
+# Import necessary packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create an engine: engine
 engine = create_engine('sqlite:///Chinook.sqlite')
 
-# Open engine in context manager
-# Perform query and save results to DataFrame: df
+# Open engine connection using a context manager
 with engine.connect() as con:
-    # Perform query and save results to DataFrame: df
+    # Define the SQL query
     query = '''
     SELECT Album.Title, Artist.Name
     FROM Album
     INNER JOIN Artist ON Album.ArtistID = Artist.ArtistID
     '''
+
+    # Execute the query and store results in DataFrame df
     rs = con.execute(query)
     df = pd.DataFrame(rs.fetchall(), columns=['Title', 'Name'])
 
-# Print head of DataFrame df
+# Print the first few rows of DataFrame df
+print("Head of DataFrame:")
 print(df.head())
 
-# Execute query and store records in DataFrame: df
-# Define the SQL query
+# Define another SQL query
 query = '''
 SELECT *
 FROM PlaylistTrack
@@ -27,6 +33,6 @@ WHERE Track.Milliseconds < 250000
 # Use read_sql_query() to execute the query and store results in DataFrame df
 df = pd.read_sql_query(query, engine)
 
-# Print head of DataFrame
+# Print the first few rows of DataFrame df
+print("Head of DataFrame:")
 print(df.head())
-
