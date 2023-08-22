7d1443b526476cace8f63241a831c38d6f01f677
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 18:52:38 +0500

    some code

--- 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/02 - Querying.py
+++ 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/02 - Querying.py
@@ -19,3 +19,17 @@ con.close()
 
 # Print head of DataFrame df
 print(df.head())
+
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT LastName, Title FROM Employee')
+    df = pd.DataFrame(rs.fetchmany(3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
