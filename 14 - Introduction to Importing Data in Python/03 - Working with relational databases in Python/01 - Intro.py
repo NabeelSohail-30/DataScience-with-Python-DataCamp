e54dbb34d3610ac576ce3c2a388dc8d55ddcf88e
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 19:12:28 +0500

    refactor

--- 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/01 - Intro.py
+++ 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/01 - Intro.py
@@ -4,12 +4,6 @@ from sqlalchemy import create_engine
 # Create engine: engine
 engine = create_engine('sqlite:///Chinook.sqlite')
 
-# Import necessary module
-from sqlalchemy import create_engine
-
-# Create engine: engine
-engine = create_engine('sqlite:///Chinook.sqlite')
-
 # Save the table names to a list: table_names
 table_names = engine.table_names()
 
