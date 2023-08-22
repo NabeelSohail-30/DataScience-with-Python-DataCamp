49ff31a638479ff500d2b6b565e5d9c6b122638c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 19:14:39 +0500

    refactor

--- 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/03 - Query with Pandas.py
+++ 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/03 - Query with Pandas.py
@@ -1,37 +1,42 @@
-# Import packages
+# Import necessary packages
 from sqlalchemy import create_engine
 import pandas as pd
 
-# Create engine: engine
+# Create an engine: engine
 engine = create_engine('sqlite:///Chinook.sqlite')
 
-# Execute query and store records in DataFrame: df
+# Execute query and store records in a DataFrame: df
 df = pd.read_sql_query('SELECT * FROM Album', engine)
 
-# Print head of DataFrame
+# Print the first few rows of the DataFrame
+print("Head of DataFrame:")
 print(df.head())
 
-# Open engine in context manager and store query result in df1
+# Open engine connection using a context manager and store query result in df1
 with engine.connect() as con:
+    # Execute SQL query and store results in rs
     rs = con.execute("SELECT * FROM Album")
+
+    # Fetch all rows and store in df1
     df1 = pd.DataFrame(rs.fetchall())
     df1.columns = rs.keys()
 
 # Confirm that both methods yield the same result
-print(df.equals(df1))
+print("Results are equal:", df.equals(df1))
 
-# Import packages
+# Import necessary packages
 from sqlalchemy import create_engine
 import pandas as pd
 
-# Create engine: engine
+# Create an engine: engine
 engine = create_engine('sqlite:///Chinook.sqlite')
 
-# Execute query and store records in DataFrame: df
+# Execute query and store records in a DataFrame: df
 df = pd.read_sql_query(
     "SELECT * FROM Employee WHERE EmployeeId >= 6 ORDER BY BirthDate",
     engine
 )
 
-# Print head of DataFrame
+# Print the first few rows of the DataFrame
+print("Head of DataFrame:")
 print(df.head())
