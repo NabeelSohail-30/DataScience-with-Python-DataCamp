3ae92f068087e95b46c14f1de5c455fc1882fbde
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 18:57:34 +0500

    some code

--- 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/02 - Querying.py
+++ 14 - Introduction to Importing Data in Python/03 - Working with relational databases in Python/02 - Querying.py
@@ -46,3 +46,17 @@ with engine.connect() as con:
 
 # Print the head of the DataFrame df
 print(df.head())
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee ORDER BY BirthDate')
+    df = pd.DataFrame(rs.fetchall())
+
+    # Set the DataFrame's column names
+    df.columns = rs.keys()
+
+# Print head of DataFrame
+print(df.head())
