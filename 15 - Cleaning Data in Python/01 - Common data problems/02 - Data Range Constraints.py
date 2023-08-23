c20d8f92c1ac448545e8cfe0a7be06c9f4f38486
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 01:02:53 +0500

    refactoring

--- 15 - Cleaning Data in Python/01 - Common data problems/02 - Data Range Constraints.py
+++ 15 - Cleaning Data in Python/01 - Common data problems/02 - Data Range Constraints.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import datetime as dt
 
+# Load the dataset
 ride_sharing = pd.read_csv('../datasets/ride_sharing_new.csv')
 
 # Convert tire_sizes to integer
@@ -11,17 +13,18 @@ ride_sharing.loc[ride_sharing['tire_sizes'] > 27, 'tire_sizes'] = 27
 # Reconvert tire_sizes back to categorical
 ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('category')
 
-# Print tire size description
+# Print description of tire_sizes column
+print("Description of 'tire_sizes' column:")
 print(ride_sharing['tire_sizes'].describe())
 
-# Convert ride_date to date
+# Convert ride_date to date using pandas datetime
 ride_sharing['ride_dt'] = pd.to_datetime(ride_sharing['ride_date']).dt.date
 
 # Save today's date
 today = dt.date.today()
 
-# Set all in the future to today's date
+# Set all dates in the future to today's date
 ride_sharing.loc[ride_sharing['ride_dt'] > today, 'ride_dt'] = today
 
-# Print maximum of ride_dt column
-print(ride_sharing['ride_dt'].max())
+# Print the maximum date in the ride_dt column
+print("Maximum date in 'ride_dt' column:", ride_sharing['ride_dt'].max())
