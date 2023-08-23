46a6d1659b98636bd774f5db0e7bdd261a1b9abf
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 00:59:52 +0500

    refactoring

--- 15 - Cleaning Data in Python/01 - Common data problems/01 - Data Type Constraints.py
+++ 15 - Cleaning Data in Python/01 - Common data problems/01 - Data Type Constraints.py
@@ -1,27 +1,31 @@
-# Print the information of ride_sharing
+# Print the information about the DataFrame
+print("DataFrame information:")
 print(ride_sharing.info())
 
-# Print summary statistics of user_type column
+# Print summary statistics of the 'user_type' column
+print("\nSummary statistics of 'user_type' column:")
 print(ride_sharing['user_type'].describe())
 
-# Convert user_type from integer to category
+# Convert 'user_type' from integer to category
 ride_sharing['user_type_cat'] = ride_sharing['user_type'].astype('category')
 
-# Write an assert statement confirming the change
+# Assert statement to confirm the dtype change
 assert ride_sharing['user_type_cat'].dtype == 'category'
 
-# Print new summary statistics
+# Print new summary statistics of the categorical column
+print("\nSummary statistics of 'user_type_cat' column:")
 print(ride_sharing['user_type_cat'].describe())
 
-# Strip duration of minutes
+# Strip 'minutes' from 'duration' column
 ride_sharing['duration_trim'] = ride_sharing['duration'].str.strip('minutes')
 
-# Convert duration to integer
+# Convert 'duration_trim' to integer
 ride_sharing['duration_time'] = ride_sharing['duration_trim'].astype('int')
 
-# Write an assert statement making sure of conversion
+# Assert statement to confirm the conversion
 assert ride_sharing['duration_time'].dtype == 'int'
 
-# Print formed columns and calculate average ride duration
-print(ride_sharing[['duration','duration_trim','duration_time']])
-print(ride_sharing['duration_time'].mean())
+# Print the new columns and calculate average ride duration
+print("\nColumns with duration details:")
+print(ride_sharing[['duration', 'duration_trim', 'duration_time']])
+print("Average ride duration:", ride_sharing['duration_time'].mean())
