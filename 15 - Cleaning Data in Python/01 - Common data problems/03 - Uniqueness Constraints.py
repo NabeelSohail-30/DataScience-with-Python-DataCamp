2b2a1df221cff00035e4f600ef504a76d9442b44
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 00:56:57 +0500

    more code

--- 15 - Cleaning Data in Python/01 - Common data problems/03 - Uniqueness Constraints.py
+++ 15 - Cleaning Data in Python/01 - Common data problems/03 - Uniqueness Constraints.py
@@ -6,3 +6,19 @@ duplicated_rides = ride_sharing[duplicates].sort_values('ride_id')
 
 # Print relevant columns
 print(duplicated_rides[['ride_id','duration','user_birth_year']])
+
+# Drop complete duplicates from ride_sharing
+ride_dup = ride_sharing.drop_duplicates()
+
+# Create statistics dictionary for aggregation function
+statistics = {'user_birth_year': 'min', 'duration': 'mean'}
+
+# Group by ride_id and compute new statistics
+ride_unique = ride_dup.groupby('ride_id').agg(statistics).reset_index()
+
+# Find duplicated values again
+duplicates = ride_unique.duplicated(subset = 'ride_id', keep = False)
+duplicated_rides = ride_unique[duplicates == True]
+
+# Assert duplicates are processed
+assert duplicated_rides.shape[0] == 0
