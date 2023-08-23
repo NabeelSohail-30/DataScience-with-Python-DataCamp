3b5b53b5bb6272fecb734c0146772fe68293ce77
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 01:05:16 +0500

    refactoring

--- 15 - Cleaning Data in Python/01 - Common data problems/03 - Uniqueness Constraints.py
+++ 15 - Cleaning Data in Python/01 - Common data problems/03 - Uniqueness Constraints.py
@@ -1,28 +1,30 @@
 import pandas as pd
 
+# Load the dataset
 ride_sharing = pd.read_csv('../datasets/ride_sharing_new.csv')
 
-# Find duplicates
-duplicates = ride_sharing.duplicated(subset = 'ride_id', keep = False)
+# Find duplicates based on 'ride_id'
+duplicates = ride_sharing.duplicated(subset='ride_id', keep=False)
 
-# Sort your duplicated rides
+# Sort the duplicated rides based on 'ride_id'
 duplicated_rides = ride_sharing[duplicates].sort_values('ride_id')
 
-# Print relevant columns
-print(duplicated_rides[['ride_id','duration','user_birth_year']])
+# Print relevant columns of duplicated rides
+print("Relevant columns of duplicated rides:")
+print(duplicated_rides[['ride_id', 'duration', 'user_birth_year']])
 
 # Drop complete duplicates from ride_sharing
 ride_dup = ride_sharing.drop_duplicates()
 
-# Create statistics dictionary for aggregation function
+# Create a dictionary for aggregation functions
 statistics = {'user_birth_year': 'min', 'duration': 'mean'}
 
-# Group by ride_id and compute new statistics
+# Group by 'ride_id' and compute new statistics using aggregation
 ride_unique = ride_dup.groupby('ride_id').agg(statistics).reset_index()
 
 # Find duplicated values again
-duplicates = ride_unique.duplicated(subset = 'ride_id', keep = False)
-duplicated_rides = ride_unique[duplicates == True]
+duplicates = ride_unique.duplicated(subset='ride_id', keep=False)
+duplicated_rides = ride_unique[duplicates]
 
-# Assert duplicates are processed
+# Assert that duplicated rides are processed
 assert duplicated_rides.shape[0] == 0
