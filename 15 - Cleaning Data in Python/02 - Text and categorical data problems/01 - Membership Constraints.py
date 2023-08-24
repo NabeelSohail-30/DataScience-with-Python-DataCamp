e231a357e5f3e236a49c3b2c969afe60f114048b
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 17:18:56 +0500

    more code

--- 15 - Cleaning Data in Python/02 - Text and categorical data problems/01 - Membership Constraints.py
+++ 15 - Cleaning Data in Python/02 - Text and categorical data problems/01 - Membership Constraints.py
@@ -17,3 +17,12 @@ print(categories)
 print('Cleanliness: ', airlines['cleanliness'].unique(), "\n")
 print('Safety: ', airlines['safety'].unique(), "\n")
 print('Satisfaction: ', airlines['satisfaction'].unique(), "\n")
+
+# Find the cleanliness category in airlines not in categories
+cat_clean = set(airlines['cleanliness']).difference(categories['cleanliness'])
+
+# Find rows with that category
+cat_clean_rows = airlines['cleanliness'].isin(cat_clean)
+
+# Print rows with inconsistent category
+print(airlines[cat_clean_rows])
