e853c51dfbb529943379805eabc77aa43509bceb
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 17:16:47 +0500

    more code

--- 15 - Cleaning Data in Python/02 - Text and categorical data problems/01 - Membership Constraints.py
+++ 15 - Cleaning Data in Python/02 - Text and categorical data problems/01 - Membership Constraints.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 airlines = pd.read_csv('../datasets/airlines_final.csv')
 
+data = {
+    'cleanliness': ['Clean', 'Average', 'Somewhat clean', 'Somewhat dirty', 'Dirty'],
+    'safety': ['Neutral', 'Very safe', 'Somewhat safe', 'Very unsafe', 'Somewhat unsafe'],
+    'satisfaction': ['Very satisfied', 'Neutral', 'Somewhat satisfied', 'Somewhat unsatisfied', 'Very unsatisfied']
+}
+
+categories = pd.DataFrame(data)
+
 # Print categories DataFrame
 print(categories)
 
