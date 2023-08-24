50b5b7fbad10173d3f29f3f1c1a59d3d2070a7b1
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 20:18:34 +0500

    more code

--- 15 - Cleaning Data in Python/02 - Text and categorical data problems/02 - Categorical Variables.py
+++ 15 - Cleaning Data in Python/02 - Text and categorical data problems/02 - Categorical Variables.py
@@ -1,29 +1,41 @@
+import pandas as pd
+import numpy as np
+
+# Load the airlines dataset
+airlines = pd.read_csv('../datasets/airlines_final.csv')
+
 # Print unique values of both columns
+print("Unique values in 'dest_region' column:")
 print(airlines['dest_region'].unique())
+print("\nUnique values in 'dest_size' column:")
 print(airlines['dest_size'].unique())
 
-# Lower dest_region column and then replace "eur" with "europe"
+# Convert 'dest_region' to lowercase and replace "eur" with "europe"
 airlines['dest_region'] = airlines['dest_region'].str.lower()
-airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+airlines['dest_region'] = airlines['dest_region'].replace({'eur': 'europe'})
 
-# Remove white spaces from `dest_size`
+# Remove white spaces from 'dest_size'
 airlines['dest_size'] = airlines['dest_size'].str.strip()
 
-# Verify changes have been effected
+# Verify changes have been applied
+print("\nUpdated 'dest_region' column:")
 print(airlines['dest_region'])
+print("\nUpdated 'dest_size' column:")
 print(airlines['dest_size'])
 
 # Create ranges for categories
 label_ranges = [0, 60, 180, np.inf]
 label_names = ['short', 'medium', 'long']
 
-# Create wait_type column
-airlines['wait_type'] = pd.cut(airlines['wait_min'], bins = label_ranges,
-                                labels = label_names)
+# Create 'wait_type' column using pd.cut()
+airlines['wait_type'] = pd.cut(airlines['wait_min'], bins=label_ranges, labels=label_names)
 
-# Create mappings and replace
-mappings = {'Monday':'weekday', 'Tuesday':'weekday', 'Wednesday': 'weekday',
-            'Thursday': 'weekday', 'Friday': 'weekday',
-            'Saturday': 'weekend', 'Sunday': 'weekend'}
+# Create mappings for day of the week
+mappings = {
+    'Monday': 'weekday', 'Tuesday': 'weekday', 'Wednesday': 'weekday',
+    'Thursday': 'weekday', 'Friday': 'weekday',
+    'Saturday': 'weekend', 'Sunday': 'weekend'
+}
 
+# Replace 'day' values using the mappings
 airlines['day_week'] = airlines['day'].replace(mappings)
