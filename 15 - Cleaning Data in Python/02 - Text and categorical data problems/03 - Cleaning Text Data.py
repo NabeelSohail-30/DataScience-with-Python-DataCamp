72a49a1b8ecd4577a6de68a4d1e939cc31072425
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 20:03:09 +0500

    more code

--- 15 - Cleaning Data in Python/02 - Text and categorical data problems/03 - Cleaning Text Data.py
+++ 15 - Cleaning Data in Python/02 - Text and categorical data problems/03 - Cleaning Text Data.py
@@ -12,3 +12,16 @@ airlines['full_name'] = airlines['full_name'].str.replace("Ms.","")
 
 # Assert that full_name has no honorifics
 assert airlines['full_name'].str.contains('Ms.|Mr.|Miss|Dr.').any() == False
+
+
+# Store length of each row in survey_response column
+resp_length = airlines['survey_response'].str.len()
+
+# Find rows in airlines where resp_length > 40
+airlines_survey = airlines[resp_length > 40]
+
+# Assert minimum survey_response length is > 40
+assert airlines_survey['survey_response'].str.len().min() > 40
+
+# Print new survey_response column
+print(airlines_survey['survey_response'])
