d9e1acccb700433b4381a37370d9ef44fcf233a0
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-24 17:12:52 +0500

    new chapter

--- 15 - Cleaning Data in Python/02 - Text and categorical data problems/01 - Membership Constraints.py
+++ 15 - Cleaning Data in Python/02 - Text and categorical data problems/01 - Membership Constraints.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
 airlines = pd.read_csv('../datasets/airlines_final.csv')
+
+# Print categories DataFrame
+print(categories)
+
+# Print unique values of survey columns in airlines
+print('Cleanliness: ', airlines['cleanliness'].unique(), "\n")
+print('Safety: ', airlines['safety'].unique(), "\n")
+print('Satisfaction: ', airlines['satisfaction'].unique(), "\n")
