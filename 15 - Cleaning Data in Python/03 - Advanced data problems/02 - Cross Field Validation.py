a4cee06421124674a1c28d055a87af4d4f6b6053
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 17:11:16 +0500

    more code

--- 15 - Cleaning Data in Python/03 - Advanced data problems/02 - Cross Field Validation.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/02 - Cross Field Validation.py
@@ -10,3 +10,17 @@ inconsistent_inv = banking[~inv_equ]
 
 # Store consistent and inconsistent data
 print("Number of inconsistent investments: ", inconsistent_inv.shape[0])
+
+# Store today's date and find ages
+today = datetime.date.today()
+ages_manual = today.year - banking['birth_date'].dt.year
+
+# Find rows where age column == ages_manual
+age_equ = banking['age'] == ages_manual
+
+# Store consistent and inconsistent data
+consistent_ages = banking[age_equ]
+inconsistent_ages = banking[~age_equ]
+
+# Store consistent and inconsistent data
+print("Number of inconsistent ages: ", inconsistent_ages.shape[0])
