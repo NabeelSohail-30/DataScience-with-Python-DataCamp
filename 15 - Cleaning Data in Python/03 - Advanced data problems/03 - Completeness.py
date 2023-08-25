c4a898099317f5f54c039d22065dae71d5688ac6
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 17:46:10 +0500

    more code

--- 15 - Cleaning Data in Python/03 - Advanced data problems/03 - Completeness.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/03 - Completeness.py
@@ -13,3 +13,15 @@ investors = banking[~banking['inv_amount'].isna()]
 banking_sorted = banking.sort_values(by='age')
 msno.matrix(banking_sorted)
 plt.show()
+
+# Drop missing values of cust_id
+banking_fullid = banking.dropna(subset=['cust_id'])
+
+# Compute estimated acct_amount
+acct_imp = banking_fullid['inv_amount'] * 5
+
+# Impute missing acct_amount with corresponding acct_imp
+banking_imputed = banking_fullid.fillna({'acct_amount': acct_imp})
+
+# Print number of missing values
+print(banking_imputed.isna().sum())
