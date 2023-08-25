dc4f30fae5a6c1f5e51cab005cedd0f1c6840599
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 16:59:21 +0500

    more code

--- 15 - Cleaning Data in Python/03 - Advanced data problems/01 - Uniformity.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/01 - Uniformity.py
@@ -19,3 +19,9 @@ banking['account_opened'] = pd.to_datetime(banking['account_opened'],
                                            infer_datetime_format = True,
                                            # Return missing value for error
                                            errors = 'coerce')
+
+# Get year of account opened
+banking['acct_year'] = banking['account_opened'].dt.strftime('%Y')
+
+# Print acct_year
+print(banking['acct_year'])
