a2761bcc646dc707c0fb5f5d1210ca352d84320e
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 17:42:05 +0500

    more code

--- 15 - Cleaning Data in Python/03 - Advanced data problems/03 - Completeness.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/03 - Completeness.py
@@ -8,3 +8,8 @@ plt.show()
 # Isolate missing and non missing values of inv_amount
 missing_investors = banking[banking['inv_amount'].isna()]
 investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values(by='age')
+msno.matrix(banking_sorted)
+plt.show()
