fbd1d3c3d7e8081e2be2d0c1df4ca362ff03a32f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 17:39:32 +0500

    more code

--- 15 - Cleaning Data in Python/03 - Advanced data problems/03 - Completeness.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/03 - Completeness.py
@@ -4,3 +4,7 @@ print(banking.isna().sum())
 # Visualize missingness matrix
 msno.matrix(banking)
 plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
