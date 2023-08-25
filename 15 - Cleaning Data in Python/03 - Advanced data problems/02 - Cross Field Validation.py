e957de29121ba48a1e7947be11589a3cd06c3881
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 17:59:24 +0500

    refactoring

--- 15 - Cleaning Data in Python/03 - Advanced data problems/02 - Cross Field Validation.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/02 - Cross Field Validation.py
@@ -1,26 +1,32 @@
+import datetime
+import pandas as pd
+
+# Load the banking dataset
+banking = pd.read_csv('../datasets/banking_dirty.csv')
+
 # Store fund columns to sum against
 fund_columns = ['fund_A', 'fund_B', 'fund_C', 'fund_D']
 
-# Find rows where fund_columns row sum == inv_amount
+# Find rows where the sum of fund_columns matches 'inv_amount'
 inv_equ = banking[fund_columns].sum(axis=1) == banking['inv_amount']
 
 # Store consistent and inconsistent data
 consistent_inv = banking[inv_equ]
 inconsistent_inv = banking[~inv_equ]
 
-# Store consistent and inconsistent data
-print("Number of inconsistent investments: ", inconsistent_inv.shape[0])
+# Print the number of inconsistent investments
+print("Number of inconsistent investments:", inconsistent_inv.shape[0])
 
-# Store today's date and find ages
+# Store today's date and calculate ages manually
 today = datetime.date.today()
 ages_manual = today.year - banking['birth_date'].dt.year
 
-# Find rows where age column == ages_manual
+# Find rows where 'age' column matches 'ages_manual'
 age_equ = banking['age'] == ages_manual
 
 # Store consistent and inconsistent data
 consistent_ages = banking[age_equ]
 inconsistent_ages = banking[~age_equ]
 
-# Store consistent and inconsistent data
-print("Number of inconsistent ages: ", inconsistent_ages.shape[0])
+# Print the number of inconsistent ages
+print("Number of inconsistent ages:", inconsistent_ages.shape[0])
