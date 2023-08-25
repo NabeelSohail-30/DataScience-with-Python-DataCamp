01385dbe3c49bf85c60b43fcdec8017f918bb6c6
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-25 16:57:29 +0500

    more code

--- 15 - Cleaning Data in Python/03 - Advanced data problems/01 - Uniformity.py
+++ 15 - Cleaning Data in Python/03 - Advanced data problems/01 - Uniformity.py
@@ -12,3 +12,10 @@ assert banking['acct_cur'].unique() == 'dollar'
 
 # Print the header of account_opened
 print(banking['account_opened'].head())
+
+# Convert account_opened to datetime
+banking['account_opened'] = pd.to_datetime(banking['account_opened'],
+                                           # Infer datetime format
+                                           infer_datetime_format = True,
+                                           # Return missing value for error
+                                           errors = 'coerce')
