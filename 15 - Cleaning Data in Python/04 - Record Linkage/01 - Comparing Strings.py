316207038780bb1238a49a5d3687f9198eab8d07
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-26 16:17:18 +0500

    more code

--- 15 - Cleaning Data in Python/04 - Record Linkage/01 - Comparing Strings.py
+++ 15 - Cleaning Data in Python/04 - Record Linkage/01 - Comparing Strings.py
@@ -21,3 +21,15 @@ matches = process.extract('italian', restaurants['cuisine_type'], limit=len(rest
 
 # Inspect the first 5 matches
 print(matches[0:5])
+
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+# Iterate through the list of matches to italian
+for match in matches:
+  # Check whether the similarity score is greater than or equal to 80
+  if match[1] >= 80:
+    # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+    restaurants.loc[restaurants['cuisine_type'] == match[0]] = 'italian'
+
+
