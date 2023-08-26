273b4405cfe40e70217fa8597aab118772e619f2
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-26 22:11:48 +0500

    more code

--- 15 - Cleaning Data in Python/04 - Record Linkage/02 - Generating Pairs.py
+++ 15 - Cleaning Data in Python/04 - Record Linkage/02 - Generating Pairs.py
@@ -9,3 +9,13 @@ pairs = indexer.index(restaurants, restaurants_new)
 
 # Create a comparison object
 comp_cl = recordlinkage.Compare()
+
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Find exact matches on city, cuisine_types
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label = 'cuisine_type')
+
+# Find similar matches of rest_name
+comp_cl.string('rest_name', 'rest_name', label='name', threshold = 0.8) 
