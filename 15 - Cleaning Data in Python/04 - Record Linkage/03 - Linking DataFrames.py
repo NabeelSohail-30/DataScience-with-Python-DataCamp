e1d80cc5c9542e68c8f42bd3b7b053bbb559093d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-26 22:49:16 +0500

    refactor

--- 15 - Cleaning Data in Python/04 - Record Linkage/03 - Linking DataFrames.py
+++ 15 - Cleaning Data in Python/04 - Record Linkage/03 - Linking DataFrames.py
@@ -1,12 +1,14 @@
-# Isolate potential matches with row sum >=3
+# Isolate potential matches with a row sum >= 3
 matches = potential_matches[potential_matches.sum(axis=1) >= 3]
 
-# Get values of second column index of matches
+# Get values of the second column index of matches
 matching_indices = matches.index.get_level_values(1)
 
-# Subset restaurants_new based on non-duplicate values
+# Subset restaurants_new based on non-duplicate values using the matching_indices
 non_dup = restaurants_new[~restaurants_new.index.get_level_values(0).isin(matching_indices)]
 
-# Append non_dup to restaurants
+# Append non_dup to restaurants to create a complete dataset
 full_restaurants = restaurants.append(non_dup)
+
+# Print the resulting complete dataset
 print(full_restaurants)
