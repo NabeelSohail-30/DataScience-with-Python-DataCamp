fbaaa2b6788359d1e3498af95391656b0f76294b
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-26 22:45:17 +0500

    refactor

--- 15 - Cleaning Data in Python/04 - Record Linkage/01 - Comparing Strings.py
+++ 15 - Cleaning Data in Python/04 - Record Linkage/01 - Comparing Strings.py
@@ -1,39 +1,45 @@
-# Import process from thefuzz
+# Import necessary libraries
 from thefuzz import process
 import pandas as pd
 
+# Load the restaurants dataset
 restaurants = pd.read_csv('../datasets/restaurants_L2_dirty.csv')
 
 # Store the unique values of cuisine_type in unique_types
 unique_types = restaurants['cuisine_type'].unique()
 
 # Calculate similarity of 'asian' to all values of unique_types
-print(process.extract('asian', unique_types, limit = len(unique_types)))
+print("Similarity to 'asian':")
+print(process.extract('asian', unique_types, limit=len(unique_types)))
 
 # Calculate similarity of 'american' to all values of unique_types
-print(process.extract('american', unique_types, limit = len(unique_types)))
+print("\nSimilarity to 'american':")
+print(process.extract('american', unique_types, limit=len(unique_types)))
 
 # Calculate similarity of 'italian' to all values of unique_types
-print(process.extract('italian', unique_types, limit = len(unique_types)))
+print("\nSimilarity to 'italian':")
+print(process.extract('italian', unique_types, limit=len(unique_types)))
 
 # Inspect the unique values of the cuisine_type column
+print("\nUnique values of cuisine_type column:")
 print(restaurants['cuisine_type'].unique())
 
 # Create a list of matches, comparing 'italian' with the cuisine_type column
 matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
 
 # Inspect the first 5 matches
+print("\nFirst 5 matches for 'italian':")
 print(matches[0:5])
 
-# Create a list of matches, comparing 'italian' with the cuisine_type column
-matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
-
-# Iterate through the list of matches to italian
+# Iterate through the list of matches for 'italian'
 for match in matches:
-  # Check whether the similarity score is greater than or equal to 80
-  if match[1] >= 80:
-    # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
-    restaurants.loc[restaurants['cuisine_type'] == match[0]] = 'italian'
+    # Check whether the similarity score is greater than or equal to 80
+    if match[1] >= 80:
+        # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+        restaurants.loc[restaurants['cuisine_type'] == match[0]] = 'italian'
+
+# List of standardized categories
+categories = ['asian', 'american', 'italian']
 
 # Iterate through categories
 for cuisine in categories:
@@ -47,5 +53,6 @@ for cuisine in categories:
             # If it is, select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
             restaurants.loc[restaurants['cuisine_type'] == match[0]] = cuisine
 
-# Inspect the final result
+# Inspect the final result of standardized cuisine types
+print("\nFinal standardized cuisine types:")
 print(restaurants['cuisine_type'].unique())
