03c3a464aef39abaa3bc59a20b611c9ed7b6db89
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-27 23:18:35 +0500

    more code

--- 16 - Working with Dates and Times in Python/01 - Dates and Calendars/02 - Math with Dates.py
+++ 16 - Working with Dates and Times in Python/01 - Dates and Calendars/02 - Math with Dates.py
@@ -27,3 +27,13 @@ print(hurricanes_each_month)
 print(dates_scrambled[0])
 print(dates_scrambled[-1])
 
+# Print the first and last scrambled dates
+print(dates_scrambled[0])
+print(dates_scrambled[-1])
+
+# Put the dates in order
+dates_ordered = sorted(dates_scrambled)
+
+# Print the first and last ordered dates
+print(dates_ordered[0])
+print(dates_ordered[-1])
