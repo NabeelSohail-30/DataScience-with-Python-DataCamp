d5ac74e2d79ed6e7a63ab0bf9c0ea5a2716e783d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-27 23:25:25 +0500

    more code

--- 16 - Working with Dates and Times in Python/01 - Dates and Calendars/03 - Dates Into String.py
+++ 16 - Working with Dates and Times in Python/01 - Dates and Calendars/03 - Dates Into String.py
@@ -17,3 +17,13 @@ andrew = date(1992, 8, 26)
 
 # Print the date in the format 'YYYY-MM'
 print(andrew.strftime('%Y-%m'))
+
+# Import date
+from datetime import date
+
+# Create a date object
+andrew = date(1992, 8, 26)
+
+# Print the date in the format 'MONTH (YYYY)'
+print(andrew.strftime('%B (%Y)'))
+
