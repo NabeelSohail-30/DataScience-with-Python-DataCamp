a5278ff947280ff17b96f7280bc6de1d4771f191
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-27 23:16:00 +0500

    more code

--- 16 - Working with Dates and Times in Python/01 - Dates and Calendars/02 - Math with Dates.py
+++ 16 - Working with Dates and Times in Python/01 - Dates and Calendars/02 - Math with Dates.py
@@ -10,3 +10,16 @@ end = date(2007, 12, 13)
 # Subtract the two dates and print the number of days
 print((end - start).days)
 
+# A dictionary to count hurricanes per calendar month
+hurricanes_each_month = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0,
+                         7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
+
+# Loop over all hurricanes
+for hurricane in florida_hurricane_dates:
+    # Pull out the month
+    month = hurricane.month
+    # Increment the count in your dictionary by one
+    hurricanes_each_month[month] += 1
+
+print(hurricanes_each_month)
+
