469c735debaf3fe083c8be4c60e7d2f8d4f28e15
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:58:12 +0500

    refactor

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
@@ -1,26 +1,20 @@
-# Import datetime
 from datetime import datetime
 import pandas as pd
 
+# Read the data into a DataFrame
 onebike_datetimes = pd.read_csv('../data/capital-onebike.csv')
 
 # Create a datetime object
 dt = datetime(2017, 10, 1, 15, 26, 26)
 
 # Print the results in ISO 8601 format
-print(dt.isoformat())
-
-# Import datetime
-from datetime import datetime
+print("Formatted datetime:", dt.isoformat())
 
 # Create a datetime object
 dt = datetime(2017, 12, 31, 15, 19, 13)
 
 # Print the results in ISO 8601 format
-print(dt.isoformat())
-
-# Import datetime
-from datetime import datetime
+print("Formatted datetime:", dt.isoformat())
 
 # Create a datetime object
 dt = datetime(2017, 12, 31, 15, 19, 13)
@@ -29,19 +23,19 @@ dt = datetime(2017, 12, 31, 15, 19, 13)
 dt_old = dt.replace(year=1917)
 
 # Print the results in ISO 8601 format
-print(dt_old)
+print("Updated year:", dt_old)
 
-# Create dictionary to hold results
+# Initialize a dictionary to hold trip counts
 trip_counts = {'AM': 0, 'PM': 0}
 
 # Loop over all trips
-for trip in onebike_datetimes:
-    # Check to see if the trip starts before noon
+for index, trip in onebike_datetimes.iterrows():
+    # Check if the trip starts before noon
     if trip['start'].hour < 12:
-        # Increment the counter for before noon
+        # Increment the counter for before noon trips
         trip_counts['AM'] += 1
     else:
-        # Increment the counter for after noon
+        # Increment the counter for after noon trips
         trip_counts['PM'] += 1
 
-print(trip_counts)
+print("Trip counts:", trip_counts)
