e3e1d1a5bff9654dc014f4b4b72144a3761bd280
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:37:54 +0500

    more code

---
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
@@ -0,0 +1,14 @@
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in YYYY-MM-DD HH:MM:SS format
+s = '2017-02-03 00:00:01'
+
+# Write a format string to parse s
+fmt = '%Y-%m-%d %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
