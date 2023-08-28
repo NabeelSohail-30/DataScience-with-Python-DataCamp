2430c6d9078a876301a0c9ce46b8de42af62762d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 23:01:24 +0500

    refactor

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/03 - Working with Durations.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/03 - Working with Durations.py
@@ -1,34 +1,46 @@
+# Import necessary libraries
 from datetime import datetime
 import pandas as pd
 
+# Load the data into a DataFrame
 onebike_datetimes = pd.read_csv('../data/capital-onebike.csv')
 
 # Initialize a list for all the trip durations
 onebike_durations = []
 
-for trip in onebike_datetimes:
-    # Create a timedelta object corresponding to the length of the trip
-    trip_duration = trip['end'] - trip['start']
+# Loop over each trip in the DataFrame
+for trip in onebike_datetimes.iterrows():
+    # Extract start and end times from the trip
+    start_time = trip[1]['start']
+    end_time = trip[1]['end']
+
+    # Convert start and end times to datetime objects
+    start_datetime = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+    end_datetime = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+
+    # Calculate the duration of the trip as a timedelta object
+    trip_duration = end_datetime - start_datetime
 
     # Get the total elapsed seconds in trip_duration
     trip_length_seconds = trip_duration.total_seconds()
 
-    # Append the results to our list
+    # Append the trip length in seconds to the list
     onebike_durations.append(trip_length_seconds)
 
-# What was the total duration of all trips?
+# Calculate the total elapsed time and number of trips
 total_elapsed_time = sum(onebike_durations)
-
-# What was the total number of trips?
 number_of_trips = len(onebike_durations)
 
-# Divide the total duration by the number of trips
-print(total_elapsed_time / number_of_trips)
+# Calculate the average trip duration
+average_trip_duration = total_elapsed_time / number_of_trips
 
-# Calculate shortest and longest trips
+# Print the average trip duration
+print("Average trip duration:", average_trip_duration, "seconds")
+
+# Calculate the shortest and longest trips
 shortest_trip = min(onebike_durations)
 longest_trip = max(onebike_durations)
 
 # Print out the results
-print("The shortest trip was " + str(shortest_trip) + " seconds")
-print("The longest trip was " + str(longest_trip) + " seconds")
+print("The shortest trip was", shortest_trip, "seconds")
+print("The longest trip was", longest_trip, "seconds")
