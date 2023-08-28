1c4136acb475b305ce1f4cf6b8bc9a3ada13f8c6
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:19:34 +0500

    more code

---
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
@@ -0,0 +1,8 @@
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 10, 1, 15, 26, 26)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
