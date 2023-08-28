5b6a0093492933d5d1abab52b9751bb4eda9a7ae
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:51:31 +0500

    more code

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/03 - Working with Durations.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/03 - Working with Durations.py
@@ -10,3 +10,12 @@ for trip in onebike_datetimes:
 
     # Append the results to our list
     onebike_durations.append(trip_length_seconds)
+
+# What was the total duration of all trips?
+total_elapsed_time = sum(onebike_durations)
+
+# What was the total number of trips?
+number_of_trips = len(onebike_durations)
+
+# Divide the total duration by the number of trips
+print(total_elapsed_time / number_of_trips)
