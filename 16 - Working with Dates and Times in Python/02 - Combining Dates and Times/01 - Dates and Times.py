18aab211b87073998bb3295ded2dfad8f620fdad
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:25:19 +0500

    more code

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
@@ -15,3 +15,15 @@ dt = datetime(2017, 12, 31, 15, 19, 13)
 
 # Print the results in ISO 8601 format
 print(dt.isoformat())
+
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Replace the year with 1917
+dt_old = dt.replace(year=1917)
+
+# Print the results in ISO 8601 format
+print(dt_old)
