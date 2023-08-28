4912bb653ddb21703c0189ca6d29c4839ffce601
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:38:44 +0500

    more code

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
@@ -12,3 +12,18 @@ d = datetime.strptime(s, fmt)
 
 # Print d
 print(d)
+
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in YYYY-MM-DD format
+s = '2030-10-15'
+
+# Write a format string to parse s
+fmt = '%Y-%m-%d'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
