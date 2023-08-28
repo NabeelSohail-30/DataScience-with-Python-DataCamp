4dd1444ba8e13c9db7ef9831a609128cded8ea2f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:20:52 +0500

    more code

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/01 - Dates and Times.py
@@ -6,3 +6,12 @@ dt = datetime(2017, 10, 1, 15, 26, 26)
 
 # Print the results in ISO 8601 format
 print(dt.isoformat())
+
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
