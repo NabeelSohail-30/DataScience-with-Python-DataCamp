daf15c34242339b8b3ee8ba4b85c2d55562141cd
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:40:23 +0500

    more code

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
@@ -27,3 +27,18 @@ d = datetime.strptime(s, fmt)
 
 # Print d
 print(d)
+
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in MM/DD/YYYY HH:MM:SS format
+s = '12/15/1986 08:00:00'
+
+# Write a format string to parse s
+fmt = '%m/%d/%Y %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
