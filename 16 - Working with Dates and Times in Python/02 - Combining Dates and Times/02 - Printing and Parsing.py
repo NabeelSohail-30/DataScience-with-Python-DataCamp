de40d91814ee40b65eafc2b4e16734e5de816344
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-28 22:44:47 +0500

    more code

--- 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
+++ 16 - Working with Dates and Times in Python/02 - Combining Dates and Times/02 - Printing and Parsing.py
@@ -56,3 +56,17 @@ for (start, end) in onebike_datetime_strings:
 
     # Append the trip
     onebike_datetimes.append(trip)
+
+
+# Import datetime
+from datetime import datetime
+
+# Pull out the start of the first trip
+first_start = onebike_datetimes[0]['start']
+
+# Format to feed to strftime()
+fmt = "%Y-%m-%dT%H:%M:%S"
+
+# Print out date with .isoformat(), then with .strftime() to compare
+print(first_start.isoformat())
+print(first_start.strftime(fmt))
