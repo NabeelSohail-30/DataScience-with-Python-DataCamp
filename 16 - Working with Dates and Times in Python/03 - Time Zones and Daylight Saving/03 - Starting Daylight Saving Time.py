98ed5ed66032cac8258681880007515b5830a054
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:30:10 +0500

    more code

---
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/03 - Starting Daylight Saving Time.py
@@ -0,0 +1,9 @@
+# Import datetime, timedelta, tz, timezone
+from datetime import datetime, timedelta, timezone
+from dateutil import tz
+
+# Start on March 12, 2017, midnight, then add 6 hours
+start = datetime(2017, 3, 12, tzinfo = tz.gettz('America/New_York'))
+end = start + timedelta(hours=6)
+print(start.isoformat() + " to " + end.isoformat())
+
