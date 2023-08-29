e32c5abf1c2e25d4cbee970fc3fa274f9f44b044
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:35:49 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/03 - Starting Daylight Saving Time.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/03 - Starting Daylight Saving Time.py
@@ -35,3 +35,14 @@ print((end - start).total_seconds()/(60*60))
 print((end.astimezone(timezone.utc) - start.astimezone(timezone.utc))\
       .total_seconds()/(60*60))
 
+# Import datetime and tz
+from datetime import datetime
+from dateutil import tz
+
+# Create starting date
+dt = datetime(2000, 3, 29, tzinfo = tz.gettz('Europe/London'))
+
+# Loop over the dates, replacing the year, and print the ISO timestamp
+for y in range(2000, 2011):
+  print(dt.replace(year=y).isoformat())
+
