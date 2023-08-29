af6c98596a3332b1028eccc37a09fe565e57cedb
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:11:52 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/02 - Timezone Database.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/02 - Timezone Database.py
@@ -36,3 +36,16 @@ notlocal = local.astimezone(ist)
 # Print them out and see the difference
 print(local.isoformat())
 print(notlocal.isoformat())
+
+# Create the timezone object
+sm = tz.gettz('Pacific/Apia')
+
+# Pull out the start of the first trip
+local = onebike_datetimes[0]['start']
+
+# What time was it in Samoa?
+notlocal = local.astimezone(sm)
+
+# Print them out and see the difference
+print(local.isoformat())
+print(notlocal.isoformat())
