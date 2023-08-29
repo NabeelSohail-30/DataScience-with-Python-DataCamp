094a689c586148f9a5e9648028cf98730d8ef69c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:37:26 +0500

    more code

---
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/04 - Ending Daylight Saving Time.py
@@ -0,0 +1,9 @@
+# Loop over trips
+for trip in onebike_datetimes:
+  # Rides with ambiguous start
+  if tz.datetime_ambiguous(trip['start']):
+    print("Ambiguous start at " + str(trip['start']))
+  # Rides with ambiguous end
+  if tz.datetime_ambiguous(trip['end']):
+    print("Ambiguous end at " + str(trip['end']))
+
