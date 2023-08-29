8da1c999bc99e04820c5574e12eb48be5bc78b1d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 21:54:13 +0500

    new chapter

---
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
@@ -0,0 +1,8 @@
+# Import datetime, timezone
+from datetime import datetime, timezone
+
+# October 1, 2017 at 15:26:26, UTC
+dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=timezone.utc)
+
+# Print results
+print(dt.isoformat())
