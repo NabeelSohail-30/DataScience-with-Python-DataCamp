1a1a9f19204adce305edb320f1989d9f530712b3
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 21:58:20 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
@@ -6,3 +6,15 @@ dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=timezone.utc)
 
 # Print results
 print(dt.isoformat())
+
+# Import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone
+
+# Create a timezone for Pacific Standard Time, or UTC-8
+pst = timezone(timedelta(hours=-8))
+
+# October 1, 2017 at 15:26:26, UTC-8
+dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=pst)
+
+# Print results
+print(dt.isoformat())
