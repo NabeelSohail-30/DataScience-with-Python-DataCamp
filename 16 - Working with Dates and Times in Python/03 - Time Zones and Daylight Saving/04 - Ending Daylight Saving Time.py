41dc1cfa503ff0496bac7e4999b8783e4e894e77
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:39:32 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/04 - Ending Daylight Saving Time.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/04 - Ending Daylight Saving Time.py
@@ -7,3 +7,18 @@ for trip in onebike_datetimes:
   if tz.datetime_ambiguous(trip['end']):
     print("Ambiguous end at " + str(trip['end']))
 
+trip_durations = []
+for trip in onebike_datetimes:
+  # When the start is later than the end, set the fold to be 1
+  if trip['start'] > trip['end']:
+    trip['end'] = tz.enfold(trip['end'])
+  # Convert to UTC
+  start = trip['start'].astimezone(tz.UTC)
+  end = trip['end'].astimezone(tz.UTC)
+
+  # Subtract the difference
+  trip_length_seconds = (end-start).total_seconds()
+  trip_durations.append(trip_length_seconds)
+
+# Take the shortest trip duration
+print("Shortest trip: " + str(min(trip_durations)))
