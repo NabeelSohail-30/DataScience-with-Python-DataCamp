faf7b58d9ce6a8cff745521fb4fb2f7c5b4f45fb
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:45:24 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/02 - Timezone Database.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/02 - Timezone Database.py
@@ -1,55 +1,56 @@
-# Import tz
+# Import necessary libraries
 from dateutil import tz
-from datetime import datetime, timezone, timedelta
 import pandas as pd
 
+# Load the data into a DataFrame
 onebike_datetimes = pd.read_csv('../data/capital-onebike.csv')
 
-# Create a timezone object for Eastern Time
+# Create a timezone object for Eastern Time (America/New_York)
 et = tz.gettz('America/New_York')
 
-# Loop over trips, updating the datetimes to be in Eastern Time
+# Loop over the first 10 trips and update their datetimes to be in Eastern Time
 for trip in onebike_datetimes[:10]:
-  # Update trip['start'] and trip['end']
-  trip['start'] = trip['start'].replace(tzinfo=et)
-  trip['end'] = trip['end'].replace(tzinfo=et)
+    trip['start'] = trip['start'].replace(tzinfo=et)
+    trip['end'] = trip['end'].replace(tzinfo=et)
 
+# Print the first trip's start time in Eastern Time
+print("Start time in Eastern Time:", onebike_datetimes[0]['start'].isoformat())
 
-# Create the timezone object
+# Create the timezone object for UK (Europe/London)
 uk = tz.gettz('Europe/London')
 
 # Pull out the start of the first trip
 local = onebike_datetimes[0]['start']
 
-# What time was it in the UK?
+# Convert the start time to UK time
 notlocal = local.astimezone(uk)
 
-# Print them out and see the difference
-print(local.isoformat())
-print(notlocal.isoformat())
+# Print the original and converted start times
+print("Original:", local.isoformat())
+print("UK Time:", notlocal.isoformat())
 
-# Create the timezone object
+# Create the timezone object for India (Asia/Kolkata)
 ist = tz.gettz('Asia/Kolkata')
 
 # Pull out the start of the first trip
 local = onebike_datetimes[0]['start']
 
-# What time was it in India?
+# Convert the start time to India time
 notlocal = local.astimezone(ist)
 
-# Print them out and see the difference
-print(local.isoformat())
-print(notlocal.isoformat())
+# Print the original and converted start times
+print("Original:", local.isoformat())
+print("India Time:", notlocal.isoformat())
 
-# Create the timezone object
+# Create the timezone object for Samoa (Pacific/Apia)
 sm = tz.gettz('Pacific/Apia')
 
 # Pull out the start of the first trip
 local = onebike_datetimes[0]['start']
 
-# What time was it in Samoa?
+# Convert the start time to Samoa time
 notlocal = local.astimezone(sm)
 
-# Print them out and see the difference
-print(local.isoformat())
-print(notlocal.isoformat())
+# Print the original and converted start times
+print("Original:", local.isoformat())
+print("Samoa Time:", notlocal.isoformat())
