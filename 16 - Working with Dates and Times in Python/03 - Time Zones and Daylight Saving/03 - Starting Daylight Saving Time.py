58f7b710e1c1f992074c9c49d2e1a1105cb9ddcb
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:31:44 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/03 - Starting Daylight Saving Time.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/03 - Starting Daylight Saving Time.py
@@ -7,3 +7,15 @@ start = datetime(2017, 3, 12, tzinfo = tz.gettz('America/New_York'))
 end = start + timedelta(hours=6)
 print(start.isoformat() + " to " + end.isoformat())
 
+# Import datetime, timedelta, tz, timezone
+from datetime import datetime, timedelta, timezone
+from dateutil import tz
+
+# Start on March 12, 2017, midnight, then add 6 hours
+start = datetime(2017, 3, 12, tzinfo = tz.gettz('America/New_York'))
+end = start + timedelta(hours=6)
+print(start.isoformat() + " to " + end.isoformat())
+
+# How many hours have elapsed?
+print((end - start).total_seconds()/(60*60))
+
