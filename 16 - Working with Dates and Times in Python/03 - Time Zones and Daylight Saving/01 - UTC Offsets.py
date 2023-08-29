5a3726d9667e0a13e512501dc5529a559975ac5f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 22:02:20 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
@@ -30,3 +30,13 @@ dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=aedt)
 
 # Print results
 print(dt.isoformat())
+
+# Create a timezone object corresponding to UTC-4
+edt = timezone(timedelta(hours=-4))
+
+# Loop over trips, updating the start and end datetimes to be in UTC-4
+for trip in onebike_datetimes[:10]:
+  # Update trip['start'] and trip['end']
+  trip['start'] = trip['start'].replace(tzinfo=edt)
+  trip['end'] = trip['end'].replace(tzinfo=edt)
+
