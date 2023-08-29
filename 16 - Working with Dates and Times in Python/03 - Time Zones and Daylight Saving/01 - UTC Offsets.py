a7d04f7c8ab52fd469c146c1a94584935e2f1949
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-29 21:59:53 +0500

    more code

--- 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
+++ 16 - Working with Dates and Times in Python/03 - Time Zones and Daylight Saving/01 - UTC Offsets.py
@@ -18,3 +18,15 @@ dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=pst)
 
 # Print results
 print(dt.isoformat())
+
+# Import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone
+
+# Create a timezone for Australian Eastern Daylight Time, or UTC+11
+aedt = timezone(timedelta(hours=11))
+
+# October 1, 2017 at 15:26:26, UTC+11
+dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=aedt)
+
+# Print results
+print(dt.isoformat())
