2c4ba6a23ce3c2f625a9b65b6d6b94770b62b91d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-30 21:57:17 +0500

    more code

--- 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/01 - Reading Date and Time.py
+++ 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/01 - Reading Date and Time.py
@@ -8,3 +8,11 @@ rides = pd.read_csv('capital-onebike.csv',
 # Print the initial (0th) row
 print(rides.iloc[0])
 
+# Subtract the start date from the end date
+ride_durations = rides['End date'] - rides['Start date']
+
+# Convert the results to seconds
+rides['Duration'] = ride_durations.dt.total_seconds()
+
+print(rides['Duration'].head())
+
