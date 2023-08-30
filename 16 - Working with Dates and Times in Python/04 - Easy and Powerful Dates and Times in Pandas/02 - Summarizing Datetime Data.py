4a2489c6ed95fd488338f06430f4aa571be0be63
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-30 22:04:27 +0500

    more code

--- 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/02 - Summarizing Datetime Data.py
+++ 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/02 - Summarizing Datetime Data.py
@@ -33,3 +33,9 @@ rides.resample('M', on = 'Start date')\
 
 # Show the results
 plt.show()
+
+# Resample rides to be monthly on the basis of Start date
+monthly_rides = rides.resample('M', on='Start date')['Member type']
+
+# Take the ratio of the .value_counts() over the total number of rides
+print(monthly_rides.value_counts() / monthly_rides.size())
