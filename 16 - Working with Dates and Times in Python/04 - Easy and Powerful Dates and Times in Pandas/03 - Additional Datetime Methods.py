e2e12790098c60d21d48b9df2a8e82b3ccde16db
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-30 22:12:47 +0500

    more code

--- 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/03 - Additional Datetime Methods.py
+++ 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/03 - Additional Datetime Methods.py
@@ -16,3 +16,15 @@ rides['Ride start weekday'] = rides['Start date'].dt.day_name()
 
 # Print the median trip time per weekday
 print(rides.groupby('Ride start weekday')['Duration'].median())
+
+# Shift the index of the end date up one; now subract it from the start date
+rides['Time since'] = rides['Start date'] - (rides['End date'].shift(1))
+
+# Move from a timedelta to a number of seconds, which is easier to work with
+rides['Time since'] = rides['Time since'].dt.total_seconds()
+
+# Resample to the month
+monthly = rides.resample('M', on = 'Start date')
+
+# Print the average hours between rides each month
+print(monthly['Time since'].mean()/(60*60))
