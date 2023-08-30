974b4de0239526803d08c56e1133fb280aaec6ea
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-30 22:08:47 +0500

    more code

--- 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/03 - Additional Datetime Methods.py
+++ 16 - Working with Dates and Times in Python/04 - Easy and Powerful Dates and Times in Pandas/03 - Additional Datetime Methods.py
@@ -1,5 +1,5 @@
-# Localize the Start date column to America/New_York
-rides['Start date'] = rides['Start date'].dt.tz_localize('America/New_York')
+# Localize the Start date column to America/New_York with the 'ambiguous' argument
+rides['Start date'] = rides['Start date'].dt.tz_localize('America/New_York', ambiguous='NaT')
 
 # Print first value
 print(rides['Start date'].iloc[0])
