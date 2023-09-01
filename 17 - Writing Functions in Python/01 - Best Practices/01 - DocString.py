f85ef09305e7c0fa4412427e23b045ab7f545464
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 00:30:02 +0500

    more code

--- 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
+++ 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
@@ -75,3 +75,24 @@ docstring = inspect.getdoc(count_letter)
 
 border = '#' * 28
 print('{}\n{}\n{}'.format(border, docstring, border))
+
+import inspect
+
+def build_tooltip(function):
+  """Create a tooltip for any function that shows the
+  function's docstring.
+
+  Args:
+    function (callable): The function we want a tooltip for.
+
+  Returns:
+    str
+  """
+  # Get the docstring for the "function" argument by using inspect
+  docstring = inspect.getdoc(function)
+  border = '#' * 28
+  return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
