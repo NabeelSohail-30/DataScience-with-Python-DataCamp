0829805ea665cc79653066c6aa3951cace85d146
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 00:45:36 +0500

    more code

---
+++ 17 - Writing Functions in Python/01 - Best Practices/02 - DRY and DO ONE THING.py
@@ -0,0 +1,18 @@
+def standardize(column):
+  """Standardize the values in a column.
+
+  Args:
+    column (pandas Series): The data to standardize.
+
+  Returns:
+    pandas Series: the values as z-scores
+  """
+  # Finish the function so that it returns the z-scores
+  z_score = (column - column.mean()) / column.std()
+  return z_score
+
+# Use the standardize() function to calculate the z-scores
+df['y1_z'] = standardize(df.y1_gpa)
+df['y2_z'] = standardize(df.y2_gpa)
+df['y3_z'] = standardize(df.y3_gpa)
+df['y4_z'] = standardize(df.y4_gpa)
