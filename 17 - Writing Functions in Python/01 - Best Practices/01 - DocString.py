3c416bc0b71e9932242db480eac8bc18663caaf1
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 00:03:50 +0500

    more code

--- 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
+++ 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
@@ -30,3 +30,16 @@ def count_letter(content, letter):
   return len([char for char in content if char == letter])
 
 
+def count_letter(content, letter):
+  """Count the number of times `letter` appears in `content`.
+
+  Args:
+    content (str): The string to search.
+    letter (str): The letter to search for.
+
+  Returns:
+    int: The number of times `letter` appears in `content`.
+  """
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('"letter" must be a single character string.')
+  return len([char for char in content if char == letter])
