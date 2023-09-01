f65c9281013d3bc595c63a0003f2fe9567bd361e
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 00:02:16 +0500

    more code

---
+++ 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
@@ -0,0 +1,18 @@
+# Add a docstring to count_letter()
+def count_letter(content, letter):
+  """
+    Count the number of times `letter` appears in `content`.
+
+    Parameters:
+        content (str): The string in which you want to count occurrences.
+        letter (str): The character you want to count.
+
+    Returns:
+        int: The number of times `letter` appears in `content`.
+
+    Raises:
+        ValueError: If `letter` is not a single character string.
+    """
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('`letter` must be a single character string.')
+  return len([char for char in content if char == letter])
