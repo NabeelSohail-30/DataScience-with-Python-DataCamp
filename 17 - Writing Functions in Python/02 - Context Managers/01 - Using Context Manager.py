ca52ad9e7dc28e2e78a375e689ebb69c9d31ccc3
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 22:26:34 +0500

    more code

--- 17 - Writing Functions in Python/02 - Context Managers/01 - Using Context Manager.py
+++ 17 - Writing Functions in Python/02 - Context Managers/01 - Using Context Manager.py
@@ -8,3 +8,15 @@ for word in text.split():
         n += 1
 
 print('Lewis Carroll uses the word "cat" {} times'.format(n))
+
+image = get_image_from_instagram()
+
+# Time how long process_with_numpy(image) takes to run
+with timer():
+    print('Numpy version')
+    process_with_numpy(image)
+
+# Time how long process_with_pytorch(image) takes to run
+with timer():
+    print('Pytorch version')
+    process_with_pytorch(image)
