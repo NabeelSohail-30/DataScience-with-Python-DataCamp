f4d71e4b925ac062e08917cf455e8e9cc017fbaa
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 22:39:04 +0500

    more code

--- 17 - Writing Functions in Python/02 - Context Managers/03 - Advanced Topics.py
+++ 17 - Writing Functions in Python/02 - Context Managers/03 - Advanced Topics.py
@@ -7,3 +7,22 @@ with stock('NVDA') as nvda:
             value = nvda.price()
             print('Logging ${:.2f} for NVDA'.format(value))
             f_out.write('{:.2f}\n'.format(value))
+
+
+def in_dir(directory):
+    """Change current working directory to `directory`,
+    allow the user to run some code, and change back.
+
+    Args:
+      directory (str): The path to a directory to work in.
+    """
+    current_dir = os.getcwd()
+    os.chdir(directory)
+
+    # Add code that lets you handle errors
+    try:
+        yield
+    # Ensure the directory is reset,
+    # whether there was an error or not
+    finally:
+        os.chdir(current_dir)
