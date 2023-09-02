193fe4a8fbcfc2484dd92226aae2b95de75deb47
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-02 22:24:12 +0500

    more code

---
+++ 17 - Writing Functions in Python/02 - Context Managers/01 - Using Context Manager.py
@@ -0,0 +1,10 @@
+# Open "alice.txt" and assign the file to "file"
+with open('alice.txt') as file:
+    text = file.read()
+
+n = 0
+for word in text.split():
+    if word.lower() in ['cat', 'cats']:
+        n += 1
+
+print('Lewis Carroll uses the word "cat" {} times'.format(n))
