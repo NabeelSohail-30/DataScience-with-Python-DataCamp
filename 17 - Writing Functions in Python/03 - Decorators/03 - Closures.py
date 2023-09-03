cfdaa44c285440e6fb5ba234a5e5a9948ecfc4ac
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:00:04 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
+++ 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
@@ -11,3 +11,20 @@ my_func = return_a_func(2, 17)
 # Show that my_func()'s closure is not None
 print(my_func.__closure__ is not None)
 
+
+def return_a_func(arg1, arg2):
+    def new_func():
+        print('arg1 was {}'.format(arg1))
+        print('arg2 was {}'.format(arg2))
+
+    return new_func
+
+
+my_func = return_a_func(2, 17)
+
+print(my_func.__closure__ is not None)
+
+# Show that there are two variables in the closure
+print(len(my_func.__closure__) == 2)
+
+
