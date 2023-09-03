665591d9a9a7c4e3cb244265ca79ca49c52cf1e8
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:02:43 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
+++ 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
@@ -68,3 +68,22 @@ def my_special_function():
 
 
 new_func()
+
+
+def my_special_function():
+    print('You are running my_special_function()')
+
+
+def get_new_func(func):
+    def call_func():
+        func()
+
+    return call_func
+
+
+new_func = get_new_func(my_special_function)
+
+# Delete my_special_function()
+del (my_special_function)
+
+new_func()
