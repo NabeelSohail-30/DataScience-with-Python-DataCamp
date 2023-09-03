cbc2dfd8798c84d8490582614f5a758437a69917
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:56:52 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/02 - Scope.py
+++ 17 - Writing Functions in Python/03 - Decorators/02 - Scope.py
@@ -32,3 +32,23 @@ for _ in range(20):
     my_function()
 
 
+def read_files():
+    file_contents = None
+
+    def save_contents(filename):
+        # Add a keyword that lets us modify file_contents
+        nonlocal file_contents
+        if file_contents is None:
+            file_contents = []
+        with open(filename) as fin:
+            file_contents.append(fin.read())
+
+    for filename in ['1984.txt', 'MobyDick.txt', 'CatsEye.txt']:
+        save_contents(filename)
+
+    return file_contents
+
+
+print('\n'.join(read_files()))
+
+
