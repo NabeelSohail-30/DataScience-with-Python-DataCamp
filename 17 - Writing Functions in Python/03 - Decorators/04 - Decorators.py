4fae01c46f6ebc5db55dea4e47a52adc268b599f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:07:42 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/04 - Decorators.py
+++ 17 - Writing Functions in Python/03 - Decorators/04 - Decorators.py
@@ -15,3 +15,18 @@ def my_function(a, b, c):
 my_function(1, 2, 3)
 
 
+def print_before_and_after(func):
+  def wrapper(*args):
+    print('Before {}'.format(func.__name__))
+    # Call the function being decorated with *args
+    func(*args)
+    print('After {}'.format(func.__name__))
+  # Return the nested function
+  return wrapper
+
+@print_before_and_after
+def multiply(a, b):
+  print(a * b)
+
+multiply(5, 10)
+
