6a5069ffd6539f3fb6702f13437d8309dacac615
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:57:49 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/02 - Scope.py
+++ 17 - Writing Functions in Python/03 - Decorators/02 - Scope.py
@@ -52,3 +52,19 @@ def read_files():
 print('\n'.join(read_files()))
 
 
+def wait_until_done():
+    def check_is_done():
+        # Add a keyword so that wait_until_done()
+        # doesn't run forever
+        global done
+        if random.random() < 0.1:
+            done = True
+
+    while not done:
+        check_is_done()
+
+
+done = False
+wait_until_done()
+
+print('Work done? {}'.format(done))
