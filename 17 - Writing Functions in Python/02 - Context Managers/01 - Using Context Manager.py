77a7d0658ddfb04aef0f822cc2cf4745a85e8c07
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:32:56 +0500

    more code

--- 17 - Writing Functions in Python/02 - Context Managers/01 - Using Context Manager.py
+++ 17 - Writing Functions in Python/02 - Context Managers/01 - Using Context Manager.py
@@ -1,4 +1,6 @@
 # Open "alice.txt" and assign the file to "file"
+from fontTools.merge import timer
+
 with open('alice.txt') as file:
     text = file.read()
 
@@ -9,14 +11,29 @@ for word in text.split():
 
 print('Lewis Carroll uses the word "cat" {} times'.format(n))
 
+
+def get_image_from_instagram():
+    return image
+
+
 image = get_image_from_instagram()
 
+
 # Time how long process_with_numpy(image) takes to run
+def process_with_numpy(image):
+    pass
+
+
 with timer():
     print('Numpy version')
     process_with_numpy(image)
 
+
 # Time how long process_with_pytorch(image) takes to run
+def process_with_pytorch(image):
+    pass
+
+
 with timer():
     print('Pytorch version')
     process_with_pytorch(image)
