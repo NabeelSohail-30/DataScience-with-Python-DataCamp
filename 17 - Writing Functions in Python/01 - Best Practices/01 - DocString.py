6c339f996ee34ac6303e2f4730cb14dd249158be
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:24:01 +0500

    more code

--- 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
+++ 17 - Writing Functions in Python/01 - Best Practices/01 - DocString.py
@@ -1,6 +1,6 @@
 # Add a docstring to count_letter()
 def count_letter(content, letter):
-  """
+    """
     Count the number of times `letter` appears in `content`.
 
     Parameters:
@@ -13,25 +13,25 @@ def count_letter(content, letter):
     Raises:
         ValueError: If `letter` is not a single character string.
     """
-  if (not isinstance(letter, str)) or len(letter) != 1:
-    raise ValueError('`letter` must be a single character string.')
-  return len([char for char in content if char == letter])
+    if (not isinstance(letter, str)) or len(letter) != 1:
+        raise ValueError('`letter` must be a single character string.')
+    return len([char for char in content if char == letter])
 
 
 def count_letter(content, letter):
-  """Count the number of times `letter` appears in `content`.
+    """Count the number of times `letter` appears in `content`.
 
   Args:
     content (str): The string to search.
     letter (str): The letter to search for.
   """
-  if (not isinstance(letter, str)) or len(letter) != 1:
-    raise ValueError('`letter` must be a single character string.')
-  return len([char for char in content if char == letter])
+    if (not isinstance(letter, str)) or len(letter) != 1:
+        raise ValueError('`letter` must be a single character string.')
+    return len([char for char in content if char == letter])
 
 
 def count_letter(content, letter):
-  """Count the number of times `letter` appears in `content`.
+    """Count the number of times `letter` appears in `content`.
 
   Args:
     content (str): The string to search.
@@ -40,13 +40,13 @@ def count_letter(content, letter):
   Returns:
     int: The number of times `letter` appears in `content`.
   """
-  if (not isinstance(letter, str)) or len(letter) != 1:
-    raise ValueError('"letter" must be a single character string.')
-  return len([char for char in content if char == letter])
+    if (not isinstance(letter, str)) or len(letter) != 1:
+        raise ValueError('"letter" must be a single character string.')
+    return len([char for char in content if char == letter])
 
 
 def count_letter(content, letter):
-  """Count the number of times `letter` appears in `content`.
+    """Count the number of times `letter` appears in `content`.
 
   Args:
     content (str): The string to search.
@@ -58,9 +58,9 @@ def count_letter(content, letter):
   Raises:
     ValueError: If `letter` is not a one-character string.
   """
-  if (not isinstance(letter, str)) or len(letter) != 1:
-    raise ValueError('`letter` must be a single character string.')
-  return len([char for char in content if char == letter])
+    if (not isinstance(letter, str)) or len(letter) != 1:
+        raise ValueError('`letter` must be a single character string.')
+    return len([char for char in content if char == letter])
 
 
 docstring = count_letter.__doc__
@@ -78,8 +78,9 @@ print('{}\n{}\n{}'.format(border, docstring, border))
 
 import inspect
 
+
 def build_tooltip(function):
-  """Create a tooltip for any function that shows the
+    """Create a tooltip for any function that shows the
   function's docstring.
 
   Args:
@@ -88,10 +89,11 @@ def build_tooltip(function):
   Returns:
     str
   """
-  # Get the docstring for the "function" argument by using inspect
-  docstring = inspect.getdoc(function)
-  border = '#' * 28
-  return '{}\n{}\n{}'.format(border, docstring, border)
+    # Get the docstring for the "function" argument by using inspect
+    docstring = inspect.getdoc(function)
+    border = '#' * 28
+    return '{}\n{}\n{}'.format(border, docstring, border)
+
 
 print(build_tooltip(count_letter))
 print(build_tooltip(range))
