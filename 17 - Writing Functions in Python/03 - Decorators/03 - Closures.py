6c239cb9019c21f26789601ac907f35239d27b88
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:01:36 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
+++ 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
@@ -46,3 +46,25 @@ closure_values = [
     my_func.__closure__[i].cell_contents for i in range(2)
 ]
 print(closure_values == [2, 17])
+
+
+def my_special_function():
+    print('You are running my_special_function()')
+
+
+def get_new_func(func):
+    def call_func():
+        func()
+
+    return call_func
+
+
+new_func = get_new_func(my_special_function)
+
+
+# Redefine my_special_function() to just print "hello"
+def my_special_function():
+    print("hello")
+
+
+new_func()
