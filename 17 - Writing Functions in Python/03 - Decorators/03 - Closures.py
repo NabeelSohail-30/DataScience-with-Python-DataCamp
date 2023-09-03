df1d9a6c31e159888f7906218e4c3d9654771061
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:03:57 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
+++ 17 - Writing Functions in Python/03 - Decorators/03 - Closures.py
@@ -87,3 +87,20 @@ new_func = get_new_func(my_special_function)
 del (my_special_function)
 
 new_func()
+
+
+def my_special_function():
+    print('You are running my_special_function()')
+
+
+def get_new_func(func):
+    def call_func():
+        func()
+
+    return call_func
+
+
+# Overwrite `my_special_function` with the new function
+my_special_function = get_new_func(my_special_function)
+
+my_special_function()
