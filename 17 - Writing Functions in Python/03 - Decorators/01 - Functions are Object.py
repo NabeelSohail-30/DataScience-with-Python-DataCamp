e01104f1379c6eabf2e69579b7a45c941038b0ed
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:45:38 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/01 - Functions are Object.py
+++ 17 - Writing Functions in Python/03 - Decorators/01 - Functions are Object.py
@@ -11,5 +11,9 @@ print(data)
 
 func_name = get_user_input()
 
-# Call the chosen function and pass "data" as an argument
-function_map[func_name](data)
+# Check if func_name is a valid key in the function_map dictionary
+if func_name in function_map:
+    # Call the chosen function and pass "data" as an argument
+    function_map[func_name](data)
+else:
+    print("Invalid function name")
