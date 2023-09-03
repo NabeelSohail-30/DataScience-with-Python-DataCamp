8331e16525a243007deff4db99de75b8835d189e
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:51:06 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/01 - Functions are Object.py
+++ 17 - Writing Functions in Python/03 - Decorators/01 - Functions are Object.py
@@ -37,3 +37,10 @@ else:
   print("as_2D() looks ok")
 
 
+# Call has_docstring() on the log_product() function
+ok = has_docstring(log_product)
+
+if not ok:
+  print("log_product() doesn't have a docstring!")
+else:
+  print("log_product() looks ok")
