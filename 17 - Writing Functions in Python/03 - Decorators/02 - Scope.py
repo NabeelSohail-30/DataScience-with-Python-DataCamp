445653070d18780dc9521004ab50f0f489d1862c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:55:54 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/02 - Scope.py
+++ 17 - Writing Functions in Python/03 - Decorators/02 - Scope.py
@@ -15,4 +15,20 @@ for func in [one, two, three]:
   func()
   print(x)
 
+call_count = 0
+
+
+def my_function():
+    # Use a keyword that lets us update call_count
+    global call_count
+    call_count += 1
+
+    print("You've called my_function() {} times!".format(
+        call_count
+    ))
+
+
+for _ in range(20):
+    my_function()
+
 
