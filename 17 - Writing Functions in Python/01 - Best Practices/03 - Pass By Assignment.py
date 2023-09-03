4e9edbf7694f933098e121502c69e997916aaa03
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:21:14 +0500

    more code

--- 17 - Writing Functions in Python/01 - Best Practices/03 - Pass By Assignment.py
+++ 17 - Writing Functions in Python/01 - Best Practices/03 - Pass By Assignment.py
@@ -1,7 +1,8 @@
 import pandas
 
+
 def better_add_column(values, df=None):
-  """Add a column of `values` to a DataFrame `df`.
+    """Add a column of `values` to a DataFrame `df`.
   The column will be named "col_<n>" where "n" is
   the numerical index of the column.
 
@@ -13,8 +14,8 @@ def better_add_column(values, df=None):
   Returns:
     DataFrame
   """
-  # Update the function to create a default DataFrame
-  if df is None:
-    df = pandas.DataFrame()
-  df['col_{}'.format(len(df.columns))] = values
-  return df
+    # Update the function to create a default DataFrame
+    if df is None:
+        df = pandas.DataFrame()
+    df['col_{}'.format(len(df.columns))] = values
+    return df
