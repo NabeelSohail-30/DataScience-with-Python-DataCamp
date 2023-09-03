97dcf869fbe8f36c4de8bf398d0d778e8d8e1225
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 22:52:39 +0500

    more code

--- 17 - Writing Functions in Python/03 - Decorators/01 - Functions are Object.py
+++ 17 - Writing Functions in Python/03 - Decorators/01 - Functions are Object.py
@@ -44,3 +44,26 @@ if not ok:
   print("log_product() doesn't have a docstring!")
 else:
   print("log_product() looks ok")
+
+
+def create_math_function(func_name):
+    if func_name == 'add':
+        def add(a, b):
+            return a + b
+
+        return add
+    elif func_name == 'subtract':
+        # Define the subtract() function
+        def subtract(a, b):
+            return a - b
+
+        return subtract
+    else:
+        print("I don't know that one")
+
+
+add = create_math_function('add')
+print('5 + 2 = {}'.format(add(5, 2)))
+
+subtract = create_math_function('subtract')
+print('5 - 2 = {}'.format(subtract(5, 2)))
