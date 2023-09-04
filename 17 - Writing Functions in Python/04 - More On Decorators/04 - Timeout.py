bc3064bcd3295616cec7deb7a7027300204ae063
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 22:53:53 +0500

    more code

--- 17 - Writing Functions in Python/04 - More On Decorators/04 - Timeout.py
+++ 17 - Writing Functions in Python/04 - More On Decorators/04 - Timeout.py
@@ -16,3 +16,21 @@ def foo():
   pass
 
 print(foo.tags)
+
+
+def returns_dict(func):
+  # Complete the returns_dict() decorator
+  def wrapper(*args, **kwargs):
+    result = func(*args, **kwargs)
+    assert type(result) == dict
+    return result
+  return wrapper
+
+@returns_dict
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
