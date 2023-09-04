1371720929989ac5b7d72b5fa1d7380607c154a1
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 22:48:41 +0500

    more code

--- 17 - Writing Functions in Python/04 - More On Decorators/02 - Decorators and Metadata.py
+++ 17 - Writing Functions in Python/04 - More On Decorators/02 - Decorators and Metadata.py
@@ -153,3 +153,17 @@ def html(open_tag, close_tag):
   return decorator
 
 
+def bold(func):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    msg = func(*args, **kwargs)
+    return '<b>{}</b>'.format(msg)
+  return wrapper
+def italics(func):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    msg = func(*args, **kwargs)
+    return '<i>{}</i>'.format(msg)
+  return wrapper
+
+
