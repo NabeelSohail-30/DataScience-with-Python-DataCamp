dd5827e93fa011e59baba8429dd9e4e50294490b
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 22:51:59 +0500

    more code

--- 17 - Writing Functions in Python/04 - More On Decorators/03 - Arguments.py
+++ 17 - Writing Functions in Python/04 - More On Decorators/03 - Arguments.py
@@ -76,3 +76,12 @@ def goodbye(name):
 
 
 print(goodbye('Alice'))
+
+
+# Wrap the result of hello_goodbye() in <div> and </div>
+@html('<div>', '</div>')
+def hello_goodbye(name):
+    return '\n{}\n{}\n'.format(hello(name), goodbye(name))
+
+
+print(hello_goodbye('Alice'))
