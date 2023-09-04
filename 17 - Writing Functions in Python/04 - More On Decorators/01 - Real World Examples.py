3858dc168eb05aaf31be1de831ed620930f6b11c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 22:36:28 +0500

    more code

--- 17 - Writing Functions in Python/04 - More On Decorators/01 - Real World Examples.py
+++ 17 - Writing Functions in Python/04 - More On Decorators/01 - Real World Examples.py
@@ -21,3 +21,26 @@ print(foo(42))
 print(foo([1, 2, 3]))
 print(foo({'a': 42}))
 
+
+def counter(func):
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        # Call the function being decorated and return the result
+        return func(*args, **kwargs)
+
+    wrapper.count = 0
+    # Return the new decorated function
+    return wrapper
+
+
+# Decorate foo() with the counter() decorator
+@counter
+def foo():
+    print('calling foo()')
+
+
+foo()
+foo()
+
+print('foo() was called {} times.'.format(foo.count))
+
