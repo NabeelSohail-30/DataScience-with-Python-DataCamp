d7e67bc0782f214794d36cb5eb661c9c2c7b69ba
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 22:42:42 +0500

    more code

--- 17 - Writing Functions in Python/04 - More On Decorators/02 - Decorators and Metadata.py
+++ 17 - Writing Functions in Python/04 - More On Decorators/02 - Decorators and Metadata.py
@@ -61,4 +61,26 @@ print_sum(10, 20)
 print_sum_docstring = print_sum.__doc__
 print(print_sum_docstring)
 
+from functools import wraps
 
+
+def add_hello(func):
+    # Decorate wrapper() so that it keeps func()'s metadata
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """Print 'hello' and then call the decorated function."""
+        print('Hello')
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@add_hello
+def print_sum(a, b):
+    """Adds two numbers and prints the sum"""
+    print(a + b)
+
+
+print_sum(10, 20)
+print_sum_docstring = print_sum.__doc__
+print(print_sum_docstring)
