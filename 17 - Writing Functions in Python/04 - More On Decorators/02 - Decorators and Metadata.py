b0d95b2ced6f1c34c730b50d5cefe95372ed1248
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 22:44:08 +0500

    more code

--- 17 - Writing Functions in Python/04 - More On Decorators/02 - Decorators and Metadata.py
+++ 17 - Writing Functions in Python/04 - More On Decorators/02 - Decorators and Metadata.py
@@ -84,3 +84,23 @@ def print_sum(a, b):
 print_sum(10, 20)
 print_sum_docstring = print_sum.__doc__
 print(print_sum_docstring)
+
+
+@check_everything
+def duplicate(my_list):
+  """Return a new list that repeats the input twice"""
+  return my_list + my_list
+
+t_start = time.time()
+duplicated_list = duplicate(list(range(50)))
+t_end = time.time()
+decorated_time = t_end - t_start
+
+t_start = time.time()
+# Call the original function instead of the decorated one
+duplicated_list = duplicate.__wrapped__(list(range(50)))
+t_end = time.time()
+undecorated_time = t_end - t_start
+
+print('Decorated time: {:.5f}s'.format(decorated_time))
+print('Undecorated time: {:.5f}s'.format(undecorated_time))
