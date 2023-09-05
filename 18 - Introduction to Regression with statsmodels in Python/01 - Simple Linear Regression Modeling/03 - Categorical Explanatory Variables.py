e73d7dd1fa7a391a35da9ef07562e40380222746
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 21:32:00 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/03 - Categorical Explanatory Variables.py
+++ 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/03 - Categorical Explanatory Variables.py
@@ -18,3 +18,8 @@ sns.displot(data=taiwan_real_estate,
 # Show the plot
 plt.show()
 
+# Calculate the mean of price_twd_msq, grouped by house age
+mean_price_by_age = taiwan_real_estate.groupby('house_age_years')['price_twd_msq'].mean()
+
+# Print the result
+print(mean_price_by_age)
