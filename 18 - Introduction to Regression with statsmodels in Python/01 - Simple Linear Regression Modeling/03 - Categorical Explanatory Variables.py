79b68be0113e0acff9068e954ca5a52f74074211
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 21:33:36 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/03 - Categorical Explanatory Variables.py
+++ 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/03 - Categorical Explanatory Variables.py
@@ -23,3 +23,9 @@ mean_price_by_age = taiwan_real_estate.groupby('house_age_years')['price_twd_msq
 
 # Print the result
 print(mean_price_by_age)
+
+# Create the model, fit it
+mdl_price_vs_age = ols("price_twd_msq ~ house_age_years", data=taiwan_real_estate).fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_age.params)
