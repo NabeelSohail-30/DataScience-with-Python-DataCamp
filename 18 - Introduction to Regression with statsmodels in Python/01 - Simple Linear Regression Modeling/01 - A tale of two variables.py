099dd49db7095519e58981494618cc7a0198515f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 21:18:59 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/01 - A tale of two variables.py
+++ 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/01 - A tale of two variables.py
@@ -9,3 +9,11 @@ sns.scatterplot(x='n_convenience', y='price_twd_msq', data=taiwan_real_estate)
 
 # Show the plot
 plt.show()
+
+# Draw a trend line on the scatter plot of price_twd_msq vs. n_convenience
+sns.regplot(x="n_convenience",
+            y="price_twd_msq",
+            data=taiwan_real_estate,
+            ci=None,  # Set confidence interval (ci) to None to omit the ribbon
+            scatter_kws={'alpha': 0.5})
+
