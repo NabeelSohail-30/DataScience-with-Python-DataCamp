bd1d975fed2b127dfed108e4bc6c1ca81305ddae
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 21:28:59 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/02 - Fitting a Linear Regression.py
+++ 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/02 - Fitting a Linear Regression.py
@@ -10,3 +10,23 @@ mdl_price_vs_conv = mdl_price_vs_conv.fit()
 # Print the parameters of the fitted model
 print(mdl_price_vs_conv.params)
 
+# Histograms of price_twd_msq with 10 bins, split by the age of each house
+sns.histplot(data=taiwan_real_estate,
+             x='price_twd_msq',
+             bins=10,
+             hue='house_age_years',
+             multiple='dodge')  # Use 'dodge' to split by house_age_years into panels
+
+# Show the plot
+plt.show()
+
+
+# Histograms of price_twd_msq with 10 bins, split by the age of each house
+sns.displot(data=taiwan_real_estate,
+            x="price_twd_msq",
+            col="house_age_years",
+            bins=10)
+
+# Show the plot
+plt.show()
+
