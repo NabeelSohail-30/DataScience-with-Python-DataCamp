cd2374f8a3ac246d4ff09a1f0447eaf13be392af
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 21:23:28 +0500

    more code

---
+++ 18 - Introduction to Regression with statsmodels in Python/01 - Simple Linear Regression Modeling/02 - Fitting a Linear Regression.py
@@ -0,0 +1,12 @@
+# Import the ols function
+from statsmodels.formula.api import ols
+
+# Create the model object
+mdl_price_vs_conv = ols('price_twd_msq ~ n_convenience', data=taiwan_real_estate)
+
+# Fit the model
+mdl_price_vs_conv = mdl_price_vs_conv.fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_conv.params)
+
