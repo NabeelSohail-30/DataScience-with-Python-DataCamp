90477f7b761220a1d6867e757e5a62a3a1e132c8
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:10:32 +0500

    more code

---
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/01 - Making Predictions.py
@@ -0,0 +1,9 @@
+# Import numpy with alias np
+import numpy as np
+import pandas as pd
+
+# Create the explanatory_data
+explanatory_data = pd.DataFrame({'n_convenience': np.arange(11)})
+
+# Print it
+print(explanatory_data)
