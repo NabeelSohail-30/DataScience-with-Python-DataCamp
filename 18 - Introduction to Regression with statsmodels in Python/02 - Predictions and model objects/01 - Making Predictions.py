efc8844ad8662541768a091fabab1e23c0328dd5
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:11:45 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/01 - Making Predictions.py
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/01 - Making Predictions.py
@@ -7,3 +7,16 @@ explanatory_data = pd.DataFrame({'n_convenience': np.arange(11)})
 
 # Print it
 print(explanatory_data)
+
+# Import numpy with alias np
+import numpy as np
+import pandas as pd
+
+# Create explanatory_data
+explanatory_data = pd.DataFrame({'n_convenience': np.arange(0, 11)})
+
+# Use mdl_price_vs_conv to predict with explanatory_data, call it price_twd_msq
+price_twd_msq = mdl_price_vs_conv.predict(explanatory_data)
+
+# Print it
+print(price_twd_msq)
