2c6659d3a6fceb7cc46f0670b672fd88dc813760
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:49:28 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/04 - Transforming Variables.py
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/04 - Transforming Variables.py
@@ -83,3 +83,16 @@ prediction_data["n_clicks"] = prediction_data["qdrt_n_clicks"] ** 4
 
 # Print prediction_data
 print(prediction_data)
+
+
+# Back transform qdrt_n_clicks
+prediction_data["n_clicks"] = prediction_data["qdrt_n_clicks"] ** 4
+
+# Plot the transformed variables
+fig = plt.figure()
+sns.regplot(x="qdrt_n_impressions", y="qdrt_n_clicks", data=ad_conversion, ci=None)
+
+# Add a layer of your prediction points
+sns.scatterplot(x="qdrt_n_impressions", y="qdrt_n_clicks", data=prediction_data, color="red")
+plt.show()
+
