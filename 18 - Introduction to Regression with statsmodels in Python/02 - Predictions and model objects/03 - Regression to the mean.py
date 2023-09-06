2ecc1cfe0f099a68cd4187cb6310f0bb064b18e9
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:32:47 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/03 - Regression to the mean.py
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/03 - Regression to the mean.py
@@ -23,4 +23,13 @@ mdl_returns = ols("return_2019 ~ return_2018", data=sp500_yearly_returns).fit()
 # Print the parameters
 print(mdl_returns.params)
 
+mdl_returns = ols("return_2019 ~ return_2018", data=sp500_yearly_returns).fit()
 
+# Create a DataFrame with return_2018 at -1, 0, and 1
+explanatory_data = pd.DataFrame({'return_2018': [-1, 0, 1]})
+
+# Use mdl_returns to predict with explanatory_data
+predictions = mdl_returns.predict(explanatory_data)
+
+# Print the predictions
+print(predictions)
