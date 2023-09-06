a972a303dcaeb5c183a199ffedb773ad342d89a8
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:43:12 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/04 - Transforming Variables.py
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/04 - Transforming Variables.py
@@ -15,3 +15,42 @@ mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_e
 
 # Print the parameters
 print(mdl_price_vs_dist.params)
+
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. sqrt_dist_to_mrt_m
+mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_estate).fit()
+
+explanatory_data = pd.DataFrame({"sqrt_dist_to_mrt_m": np.sqrt(np.arange(0, 81, 10) ** 2),
+                                "dist_to_mrt_m": np.arange(0, 81, 10) ** 2})
+
+# Create prediction_data by adding a column of predictions to explantory_data
+prediction_data = explanatory_data.assign(
+    price_twd_msq = mdl_price_vs_dist.predict(explanatory_data)
+)
+
+# Print the result
+print(prediction_data)
+
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. sqrt_dist_to_mrt_m
+mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_estate).fit()
+
+# Use this explanatory data
+explanatory_data = pd.DataFrame({"sqrt_dist_to_mrt_m": np.sqrt(np.arange(0, 81, 10) ** 2),
+                                "dist_to_mrt_m": np.arange(0, 81, 10) ** 2})
+
+# Use mdl_price_vs_dist to predict explanatory_data
+prediction_data = explanatory_data.assign(
+    price_twd_msq = mdl_price_vs_dist.predict(explanatory_data)
+)
+
+fig = plt.figure()
+sns.regplot(x="sqrt_dist_to_mrt_m", y="price_twd_msq", data=taiwan_real_estate, ci=None)
+
+# Add a layer of your prediction points
+sns.scatterplot(x="sqrt_dist_to_mrt_m", y="price_twd_msq", data=prediction_data, color="red")
+plt.show()
