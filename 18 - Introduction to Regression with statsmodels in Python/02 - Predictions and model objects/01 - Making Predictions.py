1d8b2249383277115f00740f23f3c04b9b3d0047
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:17:32 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/01 - Making Predictions.py
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/01 - Making Predictions.py
@@ -8,9 +8,6 @@ explanatory_data = pd.DataFrame({'n_convenience': np.arange(11)})
 # Print it
 print(explanatory_data)
 
-# Import numpy with alias np
-import numpy as np
-import pandas as pd
 
 # Create explanatory_data
 explanatory_data = pd.DataFrame({'n_convenience': np.arange(0, 11)})
