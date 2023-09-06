bd02aeb1e2daaf4cb69a63800d37ccd571695b43
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:21:06 +0500

    more code

---
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/02 - Working with Model Objects.py
@@ -0,0 +1,11 @@
+# Print the model parameters of mdl_price_vs_conv
+print(mdl_price_vs_conv.params)
+
+# Print the fitted values of mdl_price_vs_conv
+print(mdl_price_vs_conv.fittedvalues)
+
+# Print the residuals of mdl_price_vs_conv
+print(mdl_price_vs_conv.resid)
+
+# Print a summary of mdl_price_vs_conv
+print(mdl_price_vs_conv.summary())
