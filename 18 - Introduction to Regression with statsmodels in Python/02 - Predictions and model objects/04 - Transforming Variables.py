e0160c632a83dfd9d4fc14c6e6210ffa19a36a38
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-06 22:39:10 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/04 - Transforming Variables.py
+++ 18 - Introduction to Regression with statsmodels in Python/02 - Predictions and model objects/04 - Transforming Variables.py
@@ -7,3 +7,11 @@ plt.figure()
 sns.regplot(x="sqrt_dist_to_mrt_m", y="price_twd_msq", data=taiwan_real_estate, ci=None)
 plt.show()
 
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. square root of dist_to_mrt_m using taiwan_real_estate
+mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_estate).fit()
+
+# Print the parameters
+print(mdl_price_vs_dist.params)
