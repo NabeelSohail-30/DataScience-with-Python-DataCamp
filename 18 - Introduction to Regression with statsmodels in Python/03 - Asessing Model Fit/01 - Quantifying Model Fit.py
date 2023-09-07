dcbecfce8b34bb510459850f773fe02eedab7189
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 00:45:36 +0500

    more code

---
+++ 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/01 - Quantifying Model Fit.py
@@ -0,0 +1,5 @@
+# Print a summary of mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.summary())
+
+# Print a summary of mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.summary())
