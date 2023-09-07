51efb3c842e9b2414b7f78a4f17eab39bad4e3af
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 00:49:51 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/01 - Quantifying Model Fit.py
+++ 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/01 - Quantifying Model Fit.py
@@ -3,3 +3,9 @@ print(mdl_click_vs_impression_orig.summary())
 
 # Print a summary of mdl_click_vs_impression_trans
 print(mdl_click_vs_impression_trans.summary())
+
+# Print the coeff of determination for mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.rsquared)
+
+# Print the coeff of determination for mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.rsquared)
