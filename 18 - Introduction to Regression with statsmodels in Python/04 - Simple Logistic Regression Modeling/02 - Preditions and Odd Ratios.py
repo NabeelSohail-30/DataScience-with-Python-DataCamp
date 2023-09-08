52d349f150777b0db816b982a118eb557171ba02
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 21:55:45 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
+++ 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
@@ -52,3 +52,10 @@ sns.scatterplot(x="time_since_first_purchase",
                 color="red")
 
 plt.show()
+
+# Update prediction data with odds_ratio
+prediction_data["odds_ratio"] = prediction_data["has_churned"] / \
+    (1 - prediction_data["has_churned"])
+
+# Print the head
+print(prediction_data.head())
