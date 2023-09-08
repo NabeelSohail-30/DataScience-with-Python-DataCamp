83d05c0236c7991ae21c4f769eea8380a7e48d21
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 18:56:52 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/03 - Outliers Leverage Influence.py
+++ 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/03 - Outliers Leverage Influence.py
@@ -1,2 +1,12 @@
 # Create summary_info
 summary_info = mdl_price_vs_dist.get_influence().summary_frame()
+
+# Create summary_info
+summary_info = mdl_price_vs_dist.get_influence().summary_frame()
+
+# Add the hat_diag column to taiwan_real_estate, name it leverage
+taiwan_real_estate["leverage"] = summary_info["hat_diag"]
+
+# Sort taiwan_real_estate by leverage in descending order and print the head
+print(taiwan_real_estate.sort_values("leverage", ascending=False).head())
+
