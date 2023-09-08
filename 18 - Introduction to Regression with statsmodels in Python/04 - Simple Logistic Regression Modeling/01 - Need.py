daeaadc3781ec65c8230404cce79223a94238dae
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 21:41:01 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/01 - Need.py
+++ 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/01 - Need.py
@@ -1,1 +1,20 @@
 # Why you need logistic regression
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Assuming you have your churn data loaded into a DataFrame called 'churn_data'
+
+# Create the displot with col parameter to split histograms
+sns.displot(data=churn, x="time_since_last_purchase",
+            hue="has_churned", col="has_churned", bins=20, kde=True)
+
+# Add labels and a title
+plt.xlabel("Time Since Last Purchase")
+plt.ylabel("Frequency")
+# Adjust title position
+plt.suptitle(
+    "Distribution of Time Since Last Purchase (Churn vs. No Churn)", y=1.02)
+
+# Show the plot
+plt.show()
