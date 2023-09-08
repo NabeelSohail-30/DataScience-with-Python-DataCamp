6d6c95ff58d7b4f94f24288855344dca4de72ec3
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 18:43:47 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/01 - Quantifying Model Fit.py
+++ 18 - Introduction to Regression with statsmodels in Python/03 - Asessing Model Fit/01 - Quantifying Model Fit.py
@@ -9,3 +9,17 @@ print(mdl_click_vs_impression_orig.rsquared)
 
 # Print the coeff of determination for mdl_click_vs_impression_trans
 print(mdl_click_vs_impression_trans.rsquared)
+
+# Calculate mse_orig for mdl_click_vs_impression_orig
+mse_orig = mdl_click_vs_impression_orig.mse_resid
+
+# Calculate rse_orig for mdl_click_vs_impression_orig and print it
+rse_orig = np.sqrt(mse_orig)
+print("RSE of original model: ", rse_orig)
+
+# Calculate mse_trans for mdl_click_vs_impression_trans
+mse_trans = mdl_click_vs_impression_trans.mse_resid
+
+# Calculate rse_trans for mdl_click_vs_impression_trans and print it
+rse_trans = np.sqrt(mse_trans)
+print("RSE of transformed model: ", rse_trans)
