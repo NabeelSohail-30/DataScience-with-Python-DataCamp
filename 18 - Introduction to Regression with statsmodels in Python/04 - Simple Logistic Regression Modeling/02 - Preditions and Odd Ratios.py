6938127e54f325f24a7bf28a17fb55b81269f404
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 21:54:21 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
+++ 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
@@ -1,4 +1,7 @@
 # Create prediction_data and store predictions
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 prediction_data = explanatory_data.assign(
     has_churned=mdl_churn_vs_relationship.predict(explanatory_data)
 )
@@ -29,3 +32,23 @@ prediction_data["most_likely_outcome"] = np.round(
 
 # Print the head
 print(prediction_data.head())
+
+
+# Update prediction data by adding most_likely_outcome
+prediction_data["most_likely_outcome"] = np.round(
+    prediction_data["has_churned"])
+
+# Create a scatter plot with logistic trend line (from previous exercise)
+sns.regplot(x="time_since_first_purchase",
+            y="has_churned",
+            data=churn,
+            ci=None,
+            logistic=True)
+
+# Overlay with prediction_data, colored red
+sns.scatterplot(x="time_since_first_purchase",
+                y="most_likely_outcome",
+                data=prediction_data,
+                color="red")
+
+plt.show()
