680a3c82ab9650f3fffc43a67d806ab409d45653
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 21:58:43 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
+++ 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
@@ -81,3 +81,18 @@ prediction_data["log_odds_ratio"] = np.log(prediction_data["odds_ratio"])
 
 # Print the first five lines of prediction_data
 print(prediction_data.head())
+
+# Update prediction data with log_odds_ratio
+prediction_data["log_odds_ratio"] = np.log(prediction_data["odds_ratio"])
+
+fig = plt.figure()
+
+# Update the line plot: log_odds_ratio vs. time_since_first_purchase
+sns.lineplot(x="time_since_first_purchase",
+             y="log_odds_ratio",
+             data=prediction_data)
+
+# Add a dotted horizontal line at log_odds_ratio = 0
+plt.axhline(y=0, linestyle="dotted")
+
+plt.show()
