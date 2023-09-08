e6317b352255f8ed28e645564ace5451689cb92b
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 21:50:35 +0500

    more code

---
+++ 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/02 - Preditions and Odd Ratios.py
@@ -0,0 +1,24 @@
+# Create prediction_data and store predictions
+prediction_data = explanatory_data.assign(
+    has_churned=mdl_churn_vs_relationship.predict(explanatory_data)
+)
+
+# Print the first five lines of the prediction DataFrame
+print(prediction_data.head())
+
+fig = plt.figure()
+
+# Create a scatter plot with logistic trend line
+sns.regplot(data=churn, x="time_since_first_purchase", y="has_churned",
+            logistic=True, scatter_kws={'s': 10}, line_kws={"color": "blue"})
+
+# Overlay with prediction_data, colored red
+sns.scatterplot(data=prediction_data,
+                x="time_since_first_purchase", y="has_churned", color="red")
+
+# Add labels and a title
+plt.xlabel("Time Since First Purchase")
+plt.ylabel("Has Churned")
+plt.title("Scatter Plot of Has Churned vs. Time Since First Purchase")
+
+plt.show()
