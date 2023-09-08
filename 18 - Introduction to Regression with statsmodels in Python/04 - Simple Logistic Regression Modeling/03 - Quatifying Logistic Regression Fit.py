e42831fa5dbecc61bc9f7813428f81286cfd6b12
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-08 22:05:14 +0500

    more code

--- 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/03 - Quatifying Logistic Regression Fit.py
+++ 18 - Introduction to Regression with statsmodels in Python/04 - Simple Logistic Regression Modeling/03 - Quatifying Logistic Regression Fit.py
@@ -1,4 +1,5 @@
 # Get the actual responses
+from statsmodels.graphics.mosaicplot import mosaic
 actual_response = churn["has_churned"]
 
 # Get the predicted responses
@@ -10,3 +11,15 @@ outcomes = pd.DataFrame({"actual_response": actual_response,
 
 # Print the outcomes
 print(outcomes.value_counts(sort=False))
+
+# Calculate the confusion matrix conf_matrix using .pred_table()
+conf_matrix = mdl_churn_vs_relationship.pred_table()
+
+# Print the confusion matrix
+print(conf_matrix)
+
+# Draw a mosaic plot of conf_matrix
+mosaic(conf_matrix, title='Confusion Matrix')
+
+# Show the mosaic plot
+plt.show()
