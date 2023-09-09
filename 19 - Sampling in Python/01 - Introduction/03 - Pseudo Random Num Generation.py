4c28601beeb7570309710b87c282e40cc8baed20
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 21:20:08 +0500

    code

--- 19 - Sampling in Python/01 - Introduction/03 - Pseudo Random Num Generation.py
+++ 19 - Sampling in Python/01 - Introduction/03 - Pseudo Random Num Generation.py
@@ -1,4 +1,6 @@
 # Generate random numbers from a Uniform(-3, 3)
+import numpy as np
+import matplotlib.pyplot as plt
 uniforms = np.random.uniform(low=-3, high=3, size=5000)
 
 # Print uniforms
@@ -9,3 +11,18 @@ normals = np.random.normal(loc=5, scale=2, size=5000)
 
 # Print normals
 print(normals)
+
+
+# Assuming you have already generated the 'uniforms' data
+
+# Create a histogram of the 'uniforms' data with specified bins
+plt.figure(figsize=(8, 6))  # Optional: Set the figure size
+bin_edges = np.arange(-3, 3.25, 0.25)  # Specify bin edges with a step of 0.25
+plt.hist(uniforms, bins=bin_edges, edgecolor='k')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.title('Histogram of Uniform(-3, 3)')
+plt.grid(True)
+
+# Show the histogram
+plt.show()
