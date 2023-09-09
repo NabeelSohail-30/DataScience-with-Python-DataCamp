9c9f46a7fb22ea61868e50e161193a52545ea203
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 21:12:42 +0500

    code

--- 19 - Sampling in Python/01 - Introduction/02 - Convinience Sampling.py
+++ 19 - Sampling in Python/01 - Introduction/02 - Convinience Sampling.py
@@ -15,3 +15,15 @@ plt.grid(True)
 
 # Show the histogram
 plt.show()
+
+# Create a histogram of the 'acousticness' column with specified bins
+plt.figure(figsize=(8, 6))  # Optional: Set the figure size
+bin_edges = np.arange(0, 1.01, 0.01)  # Specify bin edges with a step of 0.01
+spotify_mysterious_sample['acousticness'].hist(bins=bin_edges, edgecolor='k')
+plt.xlabel('Acousticness')
+plt.ylabel('Frequency')
+plt.title('Acousticness Distribution in Spotify Mysterious Sample')
+plt.grid(True)
+
+# Show the histogram
+plt.show()
