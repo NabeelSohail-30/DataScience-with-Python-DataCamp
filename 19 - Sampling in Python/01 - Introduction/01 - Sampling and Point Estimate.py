d4b715dad314113667122fdfed22135835873f49
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 21:03:13 +0500

    code

--- 19 - Sampling in Python/01 - Introduction/01 - Sampling and Point Estimate.py
+++ 19 - Sampling in Python/01 - Introduction/01 - Sampling and Point Estimate.py
@@ -27,3 +27,18 @@ loudness_pop = spotify_population['loudness']
 loudness_samp = loudness_pop.sample(n=100, random_state=np.random.seed(42))
 
 print(loudness_samp)
+
+# Create a pandas Series from the loudness column of spotify_population
+loudness_pop = spotify_population['loudness']
+
+# Sample 100 values of loudness_pop
+loudness_samp = loudness_pop.sample(n=100)
+
+# Calculate the mean of loudness_pop
+mean_loudness_pop = np.mean(loudness_pop)
+
+# Calculate the mean of loudness_samp
+mean_loudness_samp = np.mean(loudness_samp)
+
+print(mean_loudness_pop)
+print(mean_loudness_samp)
