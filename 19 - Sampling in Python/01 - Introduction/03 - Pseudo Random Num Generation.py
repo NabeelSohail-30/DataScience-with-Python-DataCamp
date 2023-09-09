6a774b42ee8a731bed6266a9e8feaf10bfbdb1c8
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 21:18:41 +0500

    code

--- 19 - Sampling in Python/01 - Introduction/03 - Pseudo Random Num Generation.py
+++ 19 - Sampling in Python/01 - Introduction/03 - Pseudo Random Num Generation.py
@@ -3,3 +3,9 @@ uniforms = np.random.uniform(low=-3, high=3, size=5000)
 
 # Print uniforms
 print(uniforms)
+
+# Generate random numbers from a Normal(5, 2)
+normals = np.random.normal(loc=5, scale=2, size=5000)
+
+# Print normals
+print(normals)
