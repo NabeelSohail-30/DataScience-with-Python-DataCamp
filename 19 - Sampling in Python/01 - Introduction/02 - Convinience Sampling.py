deee4c1eb8ac039909b79efa00a4f8257a8d8d8a
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 21:12:10 +0500

    code

---
+++ 19 - Sampling in Python/01 - Introduction/02 - Convinience Sampling.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Assuming you have the Spotify dataset loaded into spotify_population DataFrame
+
+# Create a histogram of the 'acousticness' column with specified bins
+plt.figure(figsize=(8, 6))  # Optional: Set the figure size
+bin_edges = np.arange(0, 1.01, 0.01)  # Specify bin edges with a step of 0.01
+spotify_population['acousticness'].hist(bins=bin_edges, edgecolor='k')
+plt.xlabel('Acousticness')
+plt.ylabel('Frequency')
+plt.title('Acousticness Distribution in Spotify Population')
+plt.grid(True)
+
+# Show the histogram
+plt.show()
