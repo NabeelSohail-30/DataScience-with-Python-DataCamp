2a8fa5f926315d72b003e0b77754ed243aa71955
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-11 22:32:58 +0500

    code

---
+++ 19 - Sampling in Python/03 - Sampling Distributions/02 - Creating a sampling distribution.py
@@ -0,0 +1,28 @@
+# Create an empty list
+import matplotlib.pyplot as plt
+mean_attritions = []
+# Loop 500 times to create 500 sample means
+for i in range(500):
+    mean_attritions.append(
+        attrition_pop.sample(n=60)['Attrition'].mean()
+    )
+
+# Print out the first few entries of the list
+print(mean_attritions[0:5])
+
+
+# Create an empty list
+mean_attritions = []
+
+# Loop 500 times to create 500 sample means
+for i in range(500):
+    mean_attritions.append(
+        attrition_pop.sample(n=60)['Attrition'].mean()
+    )
+
+# Create a histogram of the 500 sample means with 16 bins
+plt.hist(mean_attritions, bins=16, edgecolor='k')
+plt.xlabel('Mean Attrition')
+plt.ylabel('Frequency')
+plt.title('Histogram of Sample Means (500 Samples)')
+plt.show()
